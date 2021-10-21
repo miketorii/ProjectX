0f378b535f47ee4b423d578d6c4ebe330c09778f
miketorii <miketorii@gmail.com>
2021-10-21 23:06:12 +0900

    Test react event

---
+++ testevent.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Toggle extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+	  isToggleOn: true
+      };
+      this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+      this.setState(prevState =>
+	  ( {isToggleOn: !prevState.isToggleOn} )
+	);
+  }
+    
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+	    {this.state.isToggleOn ? 'On' : 'OFF' }
+      </button>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('root')
+);
+
+
