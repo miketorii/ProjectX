f32d16655cbd8528325e91c7f700b6ef5d5d5ea0
miketorii <miketorii@gmail.com>
2024-04-27 13:11:33 +0900

    add normal method

--- deeplearning/hist.py
+++ deeplearning/hist.py
@@ -2,14 +2,27 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-path = os.path.join(os.path.dirname(__file__), 'height.txt')
-xs = np.loadtxt(path)
-print(xs.shape)
+def normal(x, mu=0, sigma=1):
+    y = 1 / (np.sqrt(2*np.pi) * sigma) * np.exp(-(x-mu)**2 /(2* sigma**2))
+    return y
 
-plt.hist(xs, bins='auto', density=True)
-plt.xlabel('Height(cm)')
-plt.ylabel('Probability Density')
-plt.show()
+if __name__=="__main__":
+    path = os.path.join(os.path.dirname(__file__), 'height.txt')
+    xs = np.loadtxt(path)
+    print(xs.shape)
+
+    mu = np.mean(xs)
+    sigma = np.std(xs)
+    print("mu=", mu, "sigma=", sigma)
+
+    x = np.linspace(150, 190, 1000)
+    y = normal(x, mu, sigma)
+
+    plt.hist(xs, bins='auto', density=True)
+    plt.plot(x, y)
+    plt.xlabel('Height(cm)')
+    plt.ylabel('Probability Density')
+    plt.show()
 
 
 
