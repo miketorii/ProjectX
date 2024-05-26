11064eaa1afbaf106ccf0b07d520dc6bc4442da0
miketorii <miketorii@gmail.com>
2024-05-26 17:13:55 +0900

    add diffusion model

--- deeplearning/diffusion_model.py
+++ deeplearning/diffusion_model.py
@@ -1,5 +1,13 @@
+import math
 import torch
+import torchvision
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from torch.optim import Adam
 from torch import nn
+from tqdm import tqdm
 
 #########################################################
 #
@@ -69,26 +77,26 @@ class UNet(nn.Module):
     def forward(self, x, timesteps):
         v = pos_encoding(timesteps, self.time_embed_dim, x.device)
 
-        print("UNet in", x.shape, v.shape)
+        #print("UNet in", x.shape, v.shape)
         x1 = self.down1(x, v)
-        print("UNet down1 x1.shape", x1.shape)
+        #print("UNet down1 x1.shape", x1.shape)
         x = self.maxpool(x1)
-        print("UNet down1", x.shape)
+        #print("UNet down1", x.shape)
         x2 = self.down2(x, v)
         x = self.maxpool(x2)
-        print("UNet down2", x.shape)
+        #print("UNet down2", x.shape)
 
         x = self.bot1(x, v)
-        print("UNet bot1", x.shape)
+        #print("UNet bot1", x.shape)
 
         x = self.upsample(x)
         x = torch.cat([x,x2], dim=1)
         x = self.up2(x, v)
-        print("UNet up2", x.shape)
+        #print("UNet up2", x.shape)
         x = self.upsample(x)
         x = torch.cat([x,x1], dim=1)
         x = self.up1(x, v)
-        print("UNet up1", x.shape)
+        #print("UNet up1", x.shape)
 
         x = self.out(x)
 
@@ -96,22 +104,143 @@ class UNet(nn.Module):
 
 #########################################################
 #
+class Diffuser:
+    def __init__(self, num_timesteps=1000, beta_start=0.0001, beta_end=0.02, device="cpu"):
+        self.num_timesteps=num_timesteps
+        self.device= device
+        self.betas = torch.linspace(beta_start, beta_end, num_timesteps, device=device)
+        self.alphas = 1 - self.betas
+        self.alpha_bars = torch.cumprod(self.alphas, dim=0)
+ 
+    def add_noise(self, x_0, t):
+        T = self.num_timesteps
+        assert (t >= 1).all() and (t <= T).all()
 
+        t_idx = t-1
+        alpha_bar = self.alpha_bars[t_idx] # (N,)
+        N = alpha_bar.size(0)
+        alpha_bar = alpha_bar.view(N,1,1,1)
+
+        eps = torch.randn_like(x_0, device=self.device)
+        x_t = torch.sqrt(alpha_bar)*x_0 + torch.sqrt(1-alpha_bar)*eps
+
+        return x_t, eps
+
+    def denoise(self, model, x, t):
+        T = self.num_timesteps
+        assert (t >= 1).all() and (t <= T).all()
+
+        t_idx = t-1
+        alpha = self.alphas[t_idx] 
+        alpha_bar = self.alpha_bars[t_idx]
+        alpha_bar_prev = self.alpha_bars[t_idx-1]
+
+        N = alpha_bar.size(0)
+        alpha = alpha.view(N,1,1,1)
+        alpha_bar = alpha_bar.view(N,1,1,1)
+        alpha_bar_prev = alpha_bar_prev.view(N,1,1,1)
+
+        model.eval()
+        with torch.no_grad():
+            eps = model(x, t)
+        model.train()
+
+        noise = torch.rand_like(x, device=self.device)
+        noise[t==1] = 0
+
+        mu = (x - ( (1-alpha)/torch.sqrt(1-alpha_bar) )*eps ) / torch.sqrt(alpha)
+        std = torch.sqrt( (1-alpha) * (1-alpha_bar_prev)/(1-alpha_bar) )
+        return mu + noise * std
+    
+    def reverse_to_img(self, x):
+        x = x*255
+        x = x.clamp(0,255)
+        x = x.to(torch.uint8)
+        x = x.cpu()
+        to_pil = transforms.ToPILImage()
+        return to_pil(x)
+    
+    def sample(self, model, x_shape=(20,1,28,28)):
+        batch_size = x_shape[0]
+        x = torch.randn(x_shape, device=self.device)
+
+        for i in tqdm(range(self.num_timesteps, 0, -1)):
+            t = torch.tensor([i] * batch_size, device=self.device, dtype=torch.long)
+            x = self.denoise(model, x, t)
+
+        images = [self.reverse_to_img(x[i]) for i in range(batch_size)]
+        return images
 
 #########################################################
 #
-'''    
+def show_images(images, rows=2, cols=10):
+    fig = plt.figure(figsize=(cols, rows))
+    i = 0
+
+    for r in range(rows):
+        for c in range(cols):
+            fig.add_subplot(rows, cols, i+1)
+            plt.imshow(images[i], cmap="gray")
+            plt.axis("off")
+            i += 1
+
+    plt.show()
+
+#########################################################
+#
 if __name__ == "__main__":
     print("---start---")
-    v = pos_encoding(torch.tensor([1,2,3]), 16)
-    print(v.shape, "\n", v)
+
+    img_size = 28
+    batch_size = 128
+    num_timesteps = 1000
+    epochs = 10
+    lr = 1e-3
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    preprocess = transforms.ToTensor()
+    dataset = torchvision.datasets.MNIST(root="./data", download=True, transform=preprocess)
+    dataloader = DataLoader(dataset, batch_size=batch_size)
+
+    diffuser = Diffuser(num_timesteps, device=device)
+    model = UNet()
+    model.to(device)
+    optimizer = Adam(model.parameters(), lr=lr)
+
+    losses = []
+    for epoch in range(epochs):
+        loss_sum = 0.0
+        cnt = 0
+
+        for images, labels in tqdm(dataloader):
+            optimizer.zero_grad()
+            x = images.to(device)
+            t = torch.randint(1, num_timesteps+1, (len(x),), device=device)
+
+            x_noisy, noise = diffuser.add_noise(x,t)
+            noise_pred = model(x_noisy, t)
+            loss = F.mse_loss(noise, noise_pred)
+
+            loss.backward()
+            optimizer.step()
+
+            loss_sum += loss.item()
+            cnt += 1
+
+        loss_avg = loss_sum / cnt
+        losses.append(loss_avg)
+        print(f"Epoch {epoch} | Loss: {loss_avg}")
+    
+    plt.plot(losses)
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.show()
+
+    images = diffuser.sample(model)
+    show_images(images)
+
+
 '''
-
-#########################################################
-#
-if __name__ == "__main__":
-    print("---start---")
-
     model = UNet()
     print(model)
 
@@ -120,3 +249,11 @@ if __name__ == "__main__":
     y = model(x, timesteps*10)
 
 #    print(y.shape)
+'''
+
+'''    
+if __name__ == "__main__":
+    print("---start---")
+    v = pos_encoding(torch.tensor([1,2,3]), 16)
+    print(v.shape, "\n", v)
+'''
