aec502e3871c59a9e18f17c600668db31dde1194
miketorii <miketorii@gmail.com>
2024-07-07 13:21:32 +0900

    update numpy exec

--- python/numpy100.py
+++ python/numpy100.py
@@ -714,13 +714,7 @@ print(X)
 Z = np.logical_not([True, False])
 print(Z)
 
-'''
-
 #78
-#### 78. Consider 2 sets of points P0,P1 describing lines (2d) and a point p, 
-# how to compute distance from p to each line i (P0[i],P1[i])? (★★★)
-#`No hints provided...`
-
 def calc_distance(p0, p1, p):
     mid = np.array([ (p1[0]-p0[0])/2, (p1[1]-p0[1])/2 ] )
     d = np.sqrt( (mid[0]-p[0])**2 + (mid[1]-p[1])**2 )
@@ -747,4 +741,71 @@ print(P1)
 d = calc_distance_array(P0,P1,p)
 print(d)
 
+'''
+#79
 
+def calc_distance2(p0, p1, p):
+    mid = np.array([ (p1[0]-p0[0])/2, (p1[1]-p0[1])/2 ] )
+    d = np.sqrt( (mid[0]-p[0])**2 + (mid[1]-p[1])**2 )
+    return d 
+
+def calc_distance_array2(P0,P1,PX):
+    dis = []
+    for i in range(len(P0)):
+        d = calc_distance2(P0[i],P1[i],PX[i])
+        dis = np.append(dis,d)
+    return dis
+
+P0 = np.random.randint(0,3,(3,2))
+P1 = np.random.randint(0,3,(3,2))
+PX = np.random.randint(0,3,(3,2))
+d = calc_distance_array2(P0,P1,PX)
+print(d)
+
+
+
+
+
+
+#### 80. Consider an arbitrary array, write a function that extract a subpart with a fixed shape and centered on a given element (pad with a `fill` value when necessary) (★★★)
+#`hint: minimum maximum`
+#### 81. Consider an array Z = [1,2,3,4,5,6,7,8,9,10,11,12,13,14], how to generate an array R = [[1,2,3,4], [2,3,4,5], [3,4,5,6], ..., [11,12,13,14]]? (★★★)
+#`hint: stride_tricks.as_strided, from numpy.lib.stride_tricks import sliding_window_view (np>=1.20.0)`
+#### 82. Compute a matrix rank (★★★)
+#`hint: np.linalg.svd, np.linalg.matrix_rank`
+#### 83. How to find the most frequent value in an array?
+#`hint: np.bincount, argmax`
+#### 84. Extract all the contiguous 3x3 blocks from a random 10x10 matrix (★★★)
+#`hint: stride_tricks.as_strided, from numpy.lib.stride_tricks import sliding_window_view (np>=1.20.0)`
+#### 85. Create a 2D array subclass such that Z[i,j] == Z[j,i] (★★★)
+#`hint: class method`
+#### 86. Consider a set of p matrices with shape (n,n) and a set of p vectors with shape (n,1). How to compute the sum of of the p matrix products at once? (result has shape (n,1)) (★★★)
+#`hint: np.tensordot`
+#### 87. Consider a 16x16 array, how to get the block-sum (block size is 4x4)? (★★★)
+#`hint: np.add.reduceat, from numpy.lib.stride_tricks import sliding_window_view (np>=1.20.0)`
+#### 88. How to implement the Game of Life using numpy arrays? (★★★)
+#`No hints provided...`
+#### 89. How to get the n largest values of an array (★★★)
+#`hint: np.argsort | np.argpartition`
+#### 90. Given an arbitrary number of vectors, build the cartesian product (every combinations of every item) (★★★)
+#`hint: np.indices`
+#### 91. How to create a record array from a regular array? (★★★)
+#`hint: np.core.records.fromarrays`
+#### 92. Consider a large vector Z, compute Z to the power of 3 using 3 different methods (★★★)
+#`hint: np.power, *, np.einsum`
+#### 93. Consider two arrays A and B of shape (8,3) and (2,2). How to find rows of A that contain elements of each row of B regardless of the order of the elements in B? (★★★)
+#`hint: np.where`
+#### 94. Considering a 10x3 matrix, extract rows with unequal values (e.g. [2,2,3]) (★★★)
+#`No hints provided...`
+#### 95. Convert a vector of ints into a matrix binary representation (★★★)
+#`hint: np.unpackbits`
+#### 96. Given a two dimensional array, how to extract unique rows? (★★★)
+#`hint: np.ascontiguousarray | np.unique`
+#### 97. Considering 2 vectors A & B, write the einsum equivalent of inner, outer, sum, and mul function (★★★)
+#`hint: np.einsum`
+#### 98. Considering a path described by two vectors (X,Y), how to sample it using equidistant samples (★★★)?
+#`hint: np.cumsum, np.interp`
+#### 99. Given an integer n and a 2D array X, select from X the rows which can be interpreted as draws from a multinomial distribution with n degrees, i.e., the rows which only contain integers and which sum to n. (★★★)
+#`hint: np.logical_and.reduce, np.mod`
+#### 100. Compute bootstrapped 95% confidence intervals for the mean of a 1D array X (i.e., resample the elements of an array with replacement N times, compute the mean of each sample, and then compute percentiles over the means). (★★★)
+#`hint: np.percentile`
