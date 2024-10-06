27d62dfd576824ec2ba74fe9b52f9971113e8847
miketorii <miketorii@gmail.com>
2024-10-06 03:18:51 +0000

    add 11

---
+++ dataanalyze100/exer2.py
@@ -0,0 +1,13 @@
+import pandas as pd
+#import matplotlib.pyplot as plt
+
+uriagedata = pd.read_csv('./data/uriage.csv')
+print( uriagedata.head() )
+print( len(uriagedata) )
+
+print("===============================================")
+kokyakudata = pd.read_excel('./data/kokyaku_daicho.xlsx')
+print( kokyakudata.head() )
+print( len(kokyakudata) )
+
+print("===============================================")
