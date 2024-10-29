0144444e855bdb3d5fb3102a86b0d51251b5d253
miketorii <miketorii@gmail.com>
2024-10-29 13:03:18 +0000

    add 36

--- dataanalyze100/exer4.py
+++ dataanalyze100/exer4.py
@@ -65,6 +65,38 @@ print(customercluster.head())
 print( customercluster.groupby(["cluster","is_deleted"],as_index=False).count()[["cluster","is_deleted","customer_id"]] )
 print( customercluster.groupby(["cluster","routine_flg"],as_index=False).count()[["cluster","routine_flg","customer_id"]] )
 
+print("==========================================")
+
+uselogdata["usedate"] = pd.to_datetime( uselogdata["usedate"] )
+uselogdata["年月"] = uselogdata["usedate"].dt.strftime("%Y%m")
+uselog_months = uselogdata.groupby(["年月","customer_id"], as_index=False).count()
+uselog_months.rename(columns={"log_id":"count"}, inplace=True)
+del uselog_months["usedate"]
+print( uselog_months.head() )
+
+year_months = list( uselog_months["年月"].unique() )
+predict_data = pd.DataFrame()
+
+for i in range(6, len(year_months) ):
+    tmp = uselog_months.loc[uselog_months["年月"]==year_months[i]].copy()
+    tmp.rename(columns={"count":"count_pred"}, inplace=True)
+    for j in range(1,7):
+        tmp_before = uselog_months.loc[uselog_months["年月"]==year_months[i-j]].copy()
+        del tmp_before["年月"]
+        tmp_before.rename(columns={"count":"count_{}".format(j-1) }, inplace=True)
+        tmp = pd.merge(tmp, tmp_before, on="customer_id", how="left")
+    predict_data = pd.concat([predict_data, tmp], ignore_index=True)
+
+print( predict_data.head() )
+
+predict_data = predict_data.dropna()
+predict_data = predict_data.reset_index(drop=True)
+
+print( predict_data.head() )
+
+
+
+
 
 
 
