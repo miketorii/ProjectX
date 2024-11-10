b822afbe6feb0fc4495755adab0620abda78dd89
miketorii <miketorii@gmail.com>
2024-11-10 03:12:27 +0000

    add 50

--- dataanalyze100/exer5.py
+++ dataanalyze100/exer5.py
@@ -155,6 +155,43 @@ plt.savefig("exer5.png")
 
 print("=====================================================")
 
+count_1 = 3
+routine_flg = 1
+period = 10
+campaign_name = "入会費無料"
+class_name = "オールタイム"
+gender = "M"
+
+if campaign_name == "入会費半額":
+    campaign_name_list = [1, 0]
+elif campaign_name == "入会費無料":
+    campaign_name_list = [0, 1]
+elif campaign_name == "通常":
+    campaign_name_list = [1, 1]
+
+if class_name == "オールタイム":
+    class_name_list = [1,0]
+elif class_name == "デイタイム":
+    class_name_list = [0,1]
+elif class_name == "ナイト":
+    class_name_list = [0,0]    
+    
+if gender == "F":
+    gender_list = [1]
+elif gender == "M":
+    gender_list = [0]
+    
+input_data = [count_1, routine_flg, period]
+input_data.extend(campaign_name_list)
+input_data.extend(class_name_list)
+input_data.extend(gender_list)
+input_data = pd.DataFrame(data=[input_data], columns=X.columns)
+
+print( model.predict(input_data) )
+print( model.predict_proba(input_data) )
+    
+print("=====================================================")
+
 
 
     
