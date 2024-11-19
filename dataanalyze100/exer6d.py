2e58e69bbee43a2b184740620ca40cf561ea226c
miketorii <miketorii@gmail.com>
2024-11-19 13:40:19 +0000

    add 59

--- dataanalyze100/exer6d.py
+++ dataanalyze100/exer6d.py
@@ -13,10 +13,36 @@ def trans_cost(df_tr, df_tc):
     return cost
 
 print( trans_cost(df_tr, df_tc) )
-
         
 print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
+df_demand = pd.read_csv("./data/demand.csv")
+df_supply = pd.read_csv("./data/supply.csv")
+print( df_demand.head() )
+print( df_supply.head() )
 
+print("---demand---")
+for i in range( len(df_demand.columns) ):
+    temp_sum = sum( df_tr[ df_demand.columns[i] ] )
+
+    print("Volume to "+str(df_demand.columns[i])+" is "+str(temp_sum))    
+    print("Supply volume "+str(df_demand.iloc[0].iloc[i]))
+    
+    if temp_sum >= df_demand.iloc[0].iloc[i]:
+        print("Under supply")
+    else:
+        print("Over. Need to recalculate")
+
+print("---supply---")        
+for i in range( len(df_supply.columns) ):
+    temp_sum = sum( df_tr.loc[ df_supply.columns[i] ] )
+
+    print("Volume from "+str(df_supply.columns[i])+" is "+str(temp_sum))    
+    print("Max supply volume "+str(df_supply.iloc[0].iloc[i]))
+    
+    if temp_sum <= df_supply.iloc[0].iloc[i]:
+        print("Under supply")
+    else:
+        print("Over. Need to recalculate")        
 
 
