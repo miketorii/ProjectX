f3d6903c4153b99d43a9654825dfb53244b07745
miketorii <miketorii@gmail.com>
2024-11-20 12:40:49 +0000

    add 60

--- dataanalyze100/exer6d.py
+++ dataanalyze100/exer6d.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 df_tr = pd.read_csv("./data/trans_route.csv", index_col="工場")
@@ -44,5 +45,40 @@ for i in range( len(df_supply.columns) ):
         print("Under supply")
     else:
         print("Over. Need to recalculate")        
+
+print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+df_tr_new = pd.read_csv("./data/trans_route_new.csv", index_col="工場")
+print( df_tr_new.head() )
+
+print( trans_cost( df_tr_new, df_tc ) )
+
+def condition_demand(df_tr, df_demand):
+    print("---condition_demand----")
+    flag = np.zeros( len(df_demand.columns) )
+    for i in range( len(df_demand.columns) ):
+        temp_sum = sum( df_tr[df_demand.columns[i]] )
+        if( temp_sum >= df_demand.iloc[0].iloc[i] ):
+            flag[i] = 1
+    return flag
+    
+def condition_supply(df_tr, df_supply):
+    print("---condition_supply----")
+    flag = np.zeros( len(df_supply.columns) )
+    for i in range( len(df_supply.columns) ):
+        temp_sum = sum( df_tr.loc[df_supply.columns[i]] )
+        if( temp_sum <= df_supply.iloc[0].iloc[i] ):
+            flag[i] = 1
+    return flag
+    
+print("---new demand---")    
+print( condition_demand(df_tr_new, df_demand) )
+print("---new supply---")
+print( condition_supply(df_tr_new, df_supply) )
+
+    
+
+    
+
 
 
