5ccc8a5a7e061e8927d3b9dff26497ed6de05e7a
miketorii <miketorii@gmail.com>
2024-11-29 23:29:24 +0000

    add 72

--- dataanalyze100/exer8.py
+++ dataanalyze100/exer8.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -22,6 +23,42 @@ for i in range(NUM):
 nx.draw_networkx(G, node_color="k", edge_color="k", font_color="w")
 
 plt.savefig("exer8.png")
+
+print("----------------------------------------")
+
+def determin_link(percent):
+    rand_val = np.random.rand()
+    if rand_val <= percent:
+        return 1
+    else:
+        return 0
+
+def simulate_percolation(num, list_active, percent_percolation):
+    for i in range(num):
+        if list_active[i]==1:
+            for j in range(num):
+                node_name = "Node" + str(j)
+                if df_links[node_name].iloc[i]==1:
+                    if determin_link(percent_percolation)==1:
+                        list_active[j] = 1
+    return list_active
+
+percent_percolation=0.1
+T_NUM = 36
+NUM = len(df_links.index)
+list_active = np.zeros(NUM)
+list_active[0] = 1
+
+list_timeSeries = []
+
+for t in range(T_NUM):
+    list_active = simulate_percolation(NUM, list_active, percent_percolation)
+    list_timeSeries.append(list_active.copy())
+
+print( list_timeSeries )
+
+    
+
 
 print("----------------------------------------")
 
