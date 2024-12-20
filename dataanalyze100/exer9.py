edfe39456149ce59cd75fe8636c2949fa744af18
miketorii <miketorii@gmail.com>
2024-12-20 13:35:21 +0000

    update 86

--- dataanalyze100/exer9.py
+++ dataanalyze100/exer9.py
@@ -127,5 +127,42 @@ for k, d in enumerate(dets):
   fontType = cv2.FONT_HERSHEY_SIMPLEX
   fontSize = 1
   cv2.rectangle(img, (d.left(), d.top()), (d.right(),d.bottom()), color_f, line_w)
-  cv2.putText(img, (d.left(), d.top()), fontType, fontSize, color_f, line_w)
-                 
+  cv2.putText(img, str(k), (d.left(), d.top()), fontType, fontSize, color_f, line_w)
+
+  num_of_points_out = 17
+  num_of_points_in = shape.num_parts - num_of_points_out
+  gx_out = 0
+  gy_out = 0
+  gx_in = 0
+  gy_in = 0
+
+  for shape_point_count in range(shape.num_parts):
+    shape_point = shape.part(shape_point_count)
+
+    if shape_point_count < num_of_points_out:
+      cv2.circle(img, (shape_point.x, shape_point.y), circle_r, color_l_out, line_w)
+      gx_out = gx_out + shape_point.x/num_of_points_out
+      gy_out = gy_out + shape_point.y/num_of_points_out
+    else:
+      cv2.circle(img, (shape_point.x, shape_point.y), circle_r, color_l_in, line_w)
+      gx_in = gx_in + shape_point.x/num_of_points_in
+      gy_in = gy_in + shape_point.y/num_of_points_in
+  
+  cv2.circle(img, (int(gx_out), int(gy_out)), circle_r, (0,0,255), line_w)
+  cv2.circle(img, (int(gx_in), int(gy_in)), circle_r, (0,0,0), line_w)
+
+  theta = math.asin(2*(gx_in-gx_out)/(d.right()-d.left()))
+  radian = theta*180/math.pi
+  print(theta)
+  print(radian)
+
+  if radian<0:
+    textPrefix = " left "
+  else:
+    textPrefix = " right "
+  
+  textShow = textPrefix + str( round(abs(radian), 1)) + " deg."
+  cv2.putText(img, textShow, (d.left(), d.top()), fontType, fontSize, color_f, line_w)
+
+cv2_imshow(img)
+cv2.imwrite("/content/drive/MyDrive/chap9/temp3.jpg", img)
