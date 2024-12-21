25fefe2e7a484f1748f45246427176f2e59b64c3
miketorii <miketorii@gmail.com>
2024-12-21 00:11:09 +0000

    add 87

--- dataanalyze100/exer9.py
+++ dataanalyze100/exer9.py
@@ -166,3 +166,42 @@ for k, d in enumerate(dets):
 
 cv2_imshow(img)
 cv2.imwrite("/content/drive/MyDrive/chap9/temp3.jpg", img)
+
+######################87
+
+cap = cv2.VideoCapture("/content/drive/MyDrive/chap9/mov/mov01.avi")
+width = int( cap.get(cv2.CAP_PROP_FRAME_WIDTH) )
+height = int( cap.get(cv2.CAP_PROP_FRAME_HEIGHT) )
+
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+hogParams = { 'winStride': (8,8), 'padding': (32,32), 'scale': 1.05, 'hitThreshold': 0, 'groupThreshold': 5}
+
+movie_name = "timelapse.avi"
+fourcc =cv2.VideoWriter_fourcc('X','V','I','D')
+video = cv2.VideoWriter(movie_name, fourcc, 30, (width, height) )
+
+num = 0
+
+while(cap.isOpened()):
+  ret, frame = cap.read()
+  if ret:
+    if(num%10==0):
+      gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+      human, r = hog.detectMultiScale(gray, **hogParams)
+      if(len(human)>0):
+        for (x,y,w,h) in human:
+          cv2.rectangle(frame, (x,y), (x+w, y+h), (255,255,255), 3)
+
+      video.write(frame)
+
+  else:
+    break
+
+  num = num + 1
+
+video.release()
+cap.release()
+cv2.destroyAllWindows()
+
+print("-------------done--------------")
