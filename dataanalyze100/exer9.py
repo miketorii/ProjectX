225f4bd8d0da4bd0a7994e17ace6ba6d3c2232a5
miketorii <miketorii@gmail.com>
2024-12-23 13:53:18 +0000

    add 89

--- dataanalyze100/exer9.py
+++ dataanalyze100/exer9.py
@@ -259,3 +259,50 @@ plt.ylim(0,15)
 plt.show()
 
 
+################################### 89
+
+cap = cv2.VideoCapture("/content/drive/MyDrive/chap9/mov/mov02.avi")
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+hogParams = { 'winStride': (8,8), 'padding': (32,32), 'scale': 1.05, 'hitThreshold': 0, 'groupThreshold': 5}
+
+num = 0
+list_df2 = pd.DataFrame( columns=['time','people'] )
+
+while(cap.isOpened()):
+  ret, frame = cap.read()
+  #print(ret)
+  if ret:
+    if (num%10==0):
+      #print("--------calc-----------")
+      gray = cv2.cvtColor( frame, cv2.COLOR_BGR2GRAY)
+      human, r = hog.detectMultiScale(gray, **hogParams)
+      if(len(human)>0):
+        for (x,y,w,h) in human:
+          cv2.rectangle(frame, (x,y), (x+w, y+h), (255,255,255), 3)
+
+      tmp_se = pd.Series( [num/fps, len(human)], index=list_df2.columns )
+      #list_df = list_df.append( tmp_se, ignore_index=True )
+      list_df2 = pd.concat([list_df2, pd.DataFrame([tmp_se])], ignore_index=True)
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+  else:
+    break
+
+  num = num+1
+  #print(num)
+
+cap.release()
+cv2.destroyAllWindows()
+print("---------------done----------------")
+
+print(list_df2.head())
+
+plt.plot(list_df2['time'], list_df2['people'])
+plt.xlabel('time(sec.)')
+plt.ylabel('population')
+plt.ylim(0,15)
+plt.show()
