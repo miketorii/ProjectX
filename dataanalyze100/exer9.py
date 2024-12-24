b2146bc1844d77c9f290da904a9be484b4a2f383
miketorii <miketorii@gmail.com>
2024-12-24 13:11:04 +0000

    add 90

--- dataanalyze100/exer9.py
+++ dataanalyze100/exer9.py
@@ -306,3 +306,37 @@ plt.xlabel('time(sec.)')
 plt.ylabel('population')
 plt.ylim(0,15)
 plt.show()
+
+#################################### 90
+import numpy as np
+
+def moving_average(x, y):
+  y_conv = np.convolve(y, np.ones(5)/float(5), mode='valid')
+  x_dat = np.linspace( np.min(x), np.max(x), np.size(y_conv) )
+  return x_dat, y_conv
+
+plt.plot(list_df["time"], list_df["people"], label="raw")
+ma_x, ma_y = moving_average(list_df["time"], list_df["people"])
+plt.plot(ma_x, ma_y, label="average")
+plt.xlabel("time(sec.)")
+plt.ylabel("population")
+plt.ylim(0,15)
+plt.legend()
+plt.show()
+
+plt.plot(list_df2["time"], list_df2["people"], label="raw")
+ma_x2, ma_y2 = moving_average(list_df2["time"], list_df2["people"])
+plt.plot(ma_x2, ma_y2, label="average")
+plt.xlabel("time(sec.)")
+plt.ylabel("population")
+plt.ylim(0,15)
+plt.legend()
+plt.show()
+
+plt.plot(ma_x, ma_y, label="1st")
+plt.plot(ma_x2, ma_y2, label="2nd")
+plt.xlabel("time(sec.)")
+plt.ylabel("population")
+plt.ylim(0,15)
+plt.legend()
+plt.show()
