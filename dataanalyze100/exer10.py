af5085b7a6db04e7791ce7c4b07883f9d9bb0d6c
miketorii <miketorii@gmail.com>
2024-12-27 22:51:36 +0000

    add 92

--- dataanalyze100/exer10.py
+++ dataanalyze100/exer10.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+############### 91
+
 survey = pd.read_csv("./data/survey.csv")
 print(len(survey))
 print(survey.head())
@@ -11,5 +13,12 @@ survey = survey.dropna()
 
 num = survey.isna().sum()
 print(num)
+
+############### 92
+
+survey["comment"] = survey["comment"].str.replace("AA","")
+print( survey.head() )
+
+
 
 
