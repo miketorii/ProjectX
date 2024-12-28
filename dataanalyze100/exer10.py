1b6be280ac26c31c986c1ebfcb26789f164c19f9
miketorii <miketorii@gmail.com>
2024-12-28 13:17:28 +0000

    update 92

--- dataanalyze100/exer10.py
+++ dataanalyze100/exer10.py
@@ -6,6 +6,8 @@ survey = pd.read_csv("./data/survey.csv")
 print(len(survey))
 print(survey.head())
 
+print("------------------------------")
+
 num = survey.isna().sum()
 print(num)
 
@@ -17,7 +19,14 @@ print(num)
 ############### 92
 
 survey["comment"] = survey["comment"].str.replace("AA","")
+print("------------------------------")
 print( survey.head() )
+
+survey["comment"] = survey["comment"].str.replace("\(.+?\)","",regex=True)
+survey["comment"] = survey["comment"].str.replace("\（.+?\）","",regex=True)
+print("------------------------------")
+print( survey.head() )
+
 
 
 
