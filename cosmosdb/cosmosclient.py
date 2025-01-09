89390a994a867ba92a9cd351b8bcb733ba22bc4b
miketorii <miketorii@gmail.com>
2025-01-09 13:35:58 +0000

    update cosmosdb client

--- cosmosdb/cosmosclient.py
+++ cosmosdb/cosmosclient.py
@@ -12,13 +12,55 @@ import config
 
 ######################################
 #
-def create_items():
+def create_items(container):
     print("---create_items---")
 
-    
-print("----------------------start--------------------")
+    for i in range(10):
+        device = get_device_info(i)
+        print(device)
+        container.create_item(body=device)
 
-create_items()
+######################################
+#   
+def get_device_info(num):
+    addr4 = 100 + num
+    macaddr_base = 0xDCAB0000
+    macaddr = macaddr_base + num
+    device_id = format(macaddr,'x')
+    device = {'deviceid': device_id,
+              'modelname': 'iR-ADV C550'+str(num),
+              'ipaddr': '192.168.11.'+str(addr4),
+              'location': 'building H '+str(num)+'F',
+              'optionss': [
+                  {'adf': 1,
+                   'paper_deck': 1,
+                   'ps': 0,
+                   'finisher': 2
+                   }
+              ],
+              'status': 10000,
+              'number': num
+              }
+
+    return device
+
+######################################
+#
+def query_items(container):
+    print("---query_items---")
+
+    items = list(container.query_items(
+        query="SELECT * FROM c",
+        enable_cross_partition_query=True
+    ))
+
+    return items
+
+
+    
+######################################
+#    
+print("----------------------start--------------------")
 
 credential = DefaultAzureCredential()
 
@@ -27,6 +69,18 @@ print(endpoint)
 
 client = CosmosClient(endpoint, credential)
 
-#https://qiita.com/baku2san/items/85714f90c094cebedb44
+databasename = config.settings['database_id']
+
+
+db = client.get_database_client(databasename)
+
+containername = config.settings['container_id']
+
+container = db.get_container_client(containername)
+
+create_items(container)
+
+devices = query_items(container)
+print(devices)
 
 print("----------------------end--------------------")
