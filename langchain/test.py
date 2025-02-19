cf04232e1927fe6400d95d6f6149a7e21b72ee9b
miketorii <miketorii@gmail.com>
2025-02-19 12:27:57 +0000

    add langchain

---
+++ langchain/test.py
@@ -0,0 +1,36 @@
+import os
+from langchain_openai import AzureChatOpenAI
+
+print("----------start----------------")
+
+llm = AzureChatOpenAI(
+    azure_deployment="MyModel",  # or your deployment
+    api_version="2024-02-01",  # or your api version
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    # other params...
+)
+
+messages = [
+    (
+        "system",
+        "You are a helpful assistant that translates English to French. Translate the user sentence.",
+    ),
+    ("human", "I love programming."),
+]
+ai_msg = llm.invoke(messages)
+
+print(ai_msg)
+print(ai_msg.content)
+
+print("-------------------------------")
+
+
+print("----------end------------------")
+
+
+
+
+
