71f338891b074ccaa9d9a3117b2d4d6032e2d52e
miketorii <miketorii@gmail.com>
2025-03-30 00:36:02 +0000

    update reflector

--- langchain/reflection_manager.py
+++ langchain/reflection_manager.py
@@ -2,13 +2,17 @@ import os
 from dotenv import load_dotenv
 import json
 import uuid
+from typing import Optional
 
 import faiss
 import numpy as np
 
 from langchain_core.language_models.chat_models import BaseChatModel
-from pydantic import BaseModel, Field
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import AzureOpenAIEmbeddings
+from langchain_openai import AzureChatOpenAI
+from pydantic import BaseModel, Field
+from retry import retry
 
 from settings import Settings
 
@@ -48,6 +52,8 @@ class ReflectionManager:
         self.embeddings_dict: dict[str, list[float]] = {}
         self.index = None
         self.load_reflections()
+#        print(self.embeddings_dict)
+#        print("-----------end init ReflectionManager------")
 
     def load_reflections(self):
         print("----load_reflections----")
@@ -78,7 +84,7 @@ class ReflectionManager:
         with open(self.file_path, "w", encoding="utf-8") as file:
             json.dump(
                 [
-                    {"reflection": reflection.dict(), "embedding": embedding}
+                    {"reflection": reflection.model_dump(), "embedding": embedding}
                     for reflection, embedding in zip(
                             self.reflections.values(), self.embeddings_dict.values()
                     )
@@ -89,17 +95,72 @@ class ReflectionManager:
             )
 
         return reflection_id
-    
+
+    def get_reflection(self, reflection_id: str) -> Optional[Reflection]:
+        return self.reflections.get(reflection_id)
+
+
+    def get_relevant_reflections(self, query: str, k: int =3) -> list[Reflection]:
+        if not self.reflections or self.index is None:
+            return []
+
+        query_embedding = self.embeddings.embed_query(query)
+        try:
+            D, I = self.index.search(
+                np.array([query_embedding]).astype("float32"),
+                min(k, len(self.reflections)),
+            )
+            reflection_ids = list(self.reflections.keys())
+            
+            return [
+                self.reflections[reflection_ids[i]]
+                for i in I[0]
+                if i < len(reflection_ids)
+            ]
+        except Exception as e:
+            print(f"Error during reflection search: {e}")
+            return []
+        
 ##########################################################
 #
 #
 class TaskReflector:
     def __init__(self, llm: BaseChatModel, reflection_manager: ReflectionManager):
-        self.llm = llm
+        self.llm = llm.with_structured_output(Reflection)
         self.reflection_manager = reflection_manager
+        print("--------End init in TaskReflector-------")
         
+    def run(self, task: str, result: str) -> Reflection:
+        prompt = ChatPromptTemplate.from_template(
+            "与えられたタスクの内容:\n{task}\n\n"
+            "タスクを実行した結果:\n{result}\n\n"
+            "あなたは高度な推論能力を持つAIエージェントです。上記のタスクを実行した結果を分析し、このタスクに対するあなたの取り組みが適切だったかどうかを内省してください。\n"
+            "以下の項目に沿って、リフレクションの内容を出力してください。\n\n"
+            "リフレクション:\n"
+            "このタスクに取り組んだ際のあなたの思考プロセスや方法を振り返ってください。何か改善できる点はありましたか？\n"
+            "次に同様のタスクに取り組む際に、より良い結果を出すための教訓を2〜3文程度で簡潔に述べてください。\n\n"
+            "判定:\n"
+            " - 結果の適切性: タスクの実行結果は適切だったと思いますか？あなたの判断を真偽値で示してください。\n"
+            " - 判定の自由度: 上記判断に対するあなたの自信の度合いを0から1までの小数で示してください。\n"
+            " - 判定の理由: タスクの実行結果の適切性とそれに対する自信度について、判断に至った理由を簡潔に列挙してください。 \n\n"
+            "出力は必ず日本語で行ってください。\n\n"
+            "Tips: Make sure to answer in the correct format."
+        )
 
+        chain = prompt | self.llm
+
+        @retry(tries=5)
+        def invoke_chain() -> Reflection:
+            return chain.invoke({"task": task, "result": result})
+
+        reflection = invoke_chain()
+        reflection_id = self.reflection_manager.save_reflection(reflection)
+        reflection.id = reflection_id
+        
+        return reflection
+    
 if __name__=="__main__":
+    print("-------------------Start main-------------------------")        
     refmgr = ReflectionManager("tmp/self_reflection_db.json")
     ref_judge = ReflectionJudgement(
         needs_retry=True,
@@ -109,10 +170,35 @@ if __name__=="__main__":
     print(ref_judge)
     ref = Reflection(
         id="100",
-        task="task a",
+        task="リコーがPFUを買収した理由を100文字以内でまとめてください。",
         reflection="this is reflection",
         judgement=ref_judge        
     )
+    print("--------------------------------------------")    
     print(ref)
-    ret = refmgr.save_reflection(ref)
-    print(ret)
+    ref_id = refmgr.save_reflection(ref)
+    print("--------------------------------------------")    
+    print(f"Reference ID: {ref_id}")
+    refgot = refmgr.get_reflection(ref_id)
+    print("--------------------------------------------")    
+    print(refgot)
+
+    listRef = refmgr.get_relevant_reflections("str3")
+    print(listRef)
+
+    llm = AzureChatOpenAI(
+        azure_deployment="my-gpt-4o-1",
+        api_version="2024-08-01-preview",
+        temperature=0.5,
+        max_tokens=None,
+        timeout=None,
+        max_retries=2,
+    )
+
+    mytask="リコーがPFUを買収した理由を100文字以内でまとめてください。"
+    prev_result = "リコーはデジタルサービスの売上拡大のためPFUのスキャンソリューションを獲得したかった。"
+    taskref = TaskReflector(llm, refmgr)
+    resultref = taskref.run(mytask, prev_result)
+    print("--------------------------------------------")
+    print(resultref)
+    print("-------------------END main-------------------------")    
