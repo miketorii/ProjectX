837ccad89087233e250ade2adf24d3fce7882260
miketorii <miketorii@gmail.com>
2025-04-24 13:37:36 +0000

    add stream_mode

--- langchain/stream1.py
+++ langchain/stream1.py
@@ -1,14 +1,37 @@
 from typing import TypedDict
 from langgraph.graph import StateGraph, START
+from langgraph.types import StreamWriter
+from langchain_openai import AzureChatOpenAI
 
 class State(TypedDict):
     topic: str
     joke: str
 
+llm = AzureChatOpenAI(
+    azure_deployment="my-gpt-4o-1",
+    api_version="2024-08-01-preview",
+    temperature=0.5,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
+
 def refine_topic(state: State):
     return {"topic": state["topic"]+" and cats"}
 
+def generate_joke1(state: State):
+    return {"joke": f"This is a joke about {state['topic']} " }
+
 def generate_joke(state: State):
+    response = llm.invoke(
+        [
+            {"role": "user", "content": f"Generate a joke about {state['topic']}"}
+        ]
+    )
+    return {"joke": response.content }
+
+def generate_joke3(state: State, writer: StreamWriter):
+    writer({"custom_key": "Writing custom data while generating a joke"})
     return {"joke": f"This is a joke about {state['topic']} " }
 
 graph = StateGraph(State)
@@ -20,7 +43,11 @@ graphcomp = graph.compile()
 
 for chunk in graphcomp.stream(
         {"topic" : "ice cream"},
-        stream_mode="values"
+#        stream_mode="values"
+#        stream_mode="updates"
+#        stream_mode="debug"
+#        stream_mode="custom"
+        stream_mode="messages"         
 ):
     print(chunk)
 
