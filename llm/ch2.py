bb2d51bab9a875bb234f17e34bf2c7403484f13a
miketorii <miketorii@gmail.com>
2025-04-30 11:29:16 +0000

    add tokenizer v2

--- llm/ch2.py
+++ llm/ch2.py
@@ -2,6 +2,7 @@ import os
 import urllib.request
 import re
 from token import SimpleTokenizerV1
+from tokenv2 import SimpleTokenizerV2
 
 if not os.path.exists("the-verdict.txt"):
 #    url = ("https://github.com/rasbt/"
@@ -39,10 +40,43 @@ for i, item in enumerate(vocab.items()):
     if i >= 30:
         break
 
+print("-------------------tokenizer v1--------------------")    
 tokenizer = SimpleTokenizerV1(vocab)
 
 text = """"It's the last he painted, you know,"
            Mr.s. Gisburn said with pardonable pride."""
+
+ids = tokenizer.encode(text)
+print(ids)
+
+result = tokenizer.decode(ids)
+print(result)
+
+#########################################################3
+#
+
+print("-------------------unknown token--------------------")
+
+all_tokens = sorted(list(set(preprocessed)))
+all_tokens.extend(["<|endoftext|>","<|unk|>"])
+
+vocab = {token:integer for integer,token in enumerate(all_tokens)}
+
+vocab_size = len(vocab.items())
+print(vocab_size)
+
+for i, item in enumerate(list(vocab.items())[-5:]):
+    print(item)
+
+print("-------------------tokenizer v2--------------------")
+
+tokenizer = SimpleTokenizerV2(vocab)
+
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+
+text = " <|endoftext|> ".join((text1,text2))
+print(text)
 
 ids = tokenizer.encode(text)
 print(ids)
