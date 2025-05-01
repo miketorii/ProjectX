e2fc16a6017a940b2c710dd48dfb863d2bd451c9
miketorii <miketorii@gmail.com>
2025-05-01 13:26:05 +0000

    add gemini

---
+++ gemini/test.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
+ 
+# .envファイルの読み込み
+load_dotenv()
+ 
+# API-KEYの設定
+GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
+genai.configure(api_key=GOOGLE_API_KEY)
+#print(GOOGLE_API_KEY)
+
+gemini_pro = genai.GenerativeModel("gemini-2.0-flash")
+prompt = "こんにちは"
+response = gemini_pro.generate_content(prompt)
+print(response.text)
