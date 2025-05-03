61cc08eb2c8a177b747297460950d19dec4128b6
miketorii <miketorii@gmail.com>
2025-05-03 00:35:37 +0000

    add torch

---
+++ llm/slidewin2.py
@@ -0,0 +1,44 @@
+import os
+import re
+import importlib
+import tiktoken
+from torch.utils.data import DataLoader
+from gptdatasetv1 import GPTDatasetV1
+
+########################################################3
+#
+#
+def create_dataloader_v1(txt, batch_size=4, max_length=256,
+                         stride=128, shuffle=True, drop_last=True, num_workers=0):
+    tokenizer = tiktoken.get_encoding("gpt2")
+
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers
+    )
+
+    return dataloader
+
+########################################################3
+#
+#
+with open("the-verdict.txt","r",encoding="utf-8") as f:
+    raw_text = f.read()
+
+dataloader = create_dataloader_v1(
+    raw_text, batch_size=1, max_length=4, stride=1, shuffle=False
+)
+
+data_iter = iter(dataloader)
+first_batch = next(data_iter)
+print(first_batch)
+
+second_batch = next(data_iter)
+print(second_batch)
+
+
