5ca1bfbca8c3453c82a9d59174722319e09e1631
unknown <miketorii@gmail.com>
2025-05-18 03:11:03 +0000

    add file monitor

---
+++ cybersecurity/file_monitor0.py
@@ -0,0 +1,74 @@
+import os
+import tempfile
+import threading
+import win32con
+import win32file
+
+FILE_CREATED = 1
+FILE_DELETED = 2
+FILE_MODIFIED = 3
+FILE_RENAMED_FROM = 4
+FILE_RENAMED_TO = 5
+
+FILE_LIST_DIRECTORY = 0x0001
+PATHS = ["c:\\Users\\mtorii\\develop\\ProjectX\\cybersecurity", tempfile.gettempdir()]
+
+def monitor(path_to_watch):
+    print("---monitor--")
+    h_directory = win32file.CreateFile(
+        path_to_watch,
+        FILE_LIST_DIRECTORY,
+        win32con.FILE_SHARE_READ | win32con.FILE_SHARE_WRITE | win32con.FILE_SHARE_DELETE,
+        None,
+        win32con.OPEN_EXISTING,
+        win32con.FILE_FLAG_BACKUP_SEMANTICS,
+        None
+    )
+
+    results = win32file.ReadDirectoryChangesW(
+        h_directory,
+        1024,
+        True,
+        win32con.FILE_NOTIFY_CHANGE_ATTRIBUTES |
+        win32con.FILE_NOTIFY_CHANGE_DIR_NAME |
+        win32con.FILE_NOTIFY_CHANGE_FILE_NAME |
+        win32con.FILE_NOTIFY_CHANGE_LAST_WRITE |
+        win32con.FILE_NOTIFY_CHANGE_SECURITY |
+        win32con.FILE_NOTIFY_CHANGE_SIZE,                
+        None,
+        None
+    )
+    
+    for action, file_name in results:
+        full_filename = os.path.join(path_to_watch, file_name)
+        if action == FILE_CREATED:
+            print(f'[*] Created {full_filename}')
+            
+        elif action == FILE_DELETED:
+            print(f'[-] Deleted {full_filename}')
+            
+        elif action == FILE_MODIFIED:
+            print(f'[*] Modified {full_filename}')
+            try:
+                print('[vvv] Dumping contents ...')
+                with open(full_filename) as f:
+                    contents = f.read()
+                print(contents)
+                print('[^^^] Dump complete.')
+            except Exception as e:
+                print(f'[!!!] Dump failed. {e}')
+            
+        elif action == FILE_RENAMED_FROM:
+            print(f'[>] Renamed from {full_filename}')            
+        elif action == FILE_RENAMED_TO:
+            print(f'[<] Renamed to {full_filename}')
+        else:
+            print(f'[?] Renamed to {full_filename}')
+            
+if __name__ == "__main__":
+    print("------------Start-------------")
+
+    filepath = "c:\\Users\\mtorii\\develop\\ProjectX\\cybersecurity"
+    monitor(filepath)
+    
+    print("-------------End--------------")    
