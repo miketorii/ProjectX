a31845acd7ac4b918af38c1076e218806730721f
miketorii <miketorii@gmail.com>
2025-06-07 11:14:56 +0000

    writing

--- cybersecurity/proxy.py
+++ cybersecurity/proxy.py
@@ -6,10 +6,46 @@ HEX_FILTER = ''.join(
     [(len(repr(chr(i)))==3) and chr(i) or '.' for i in range(256)]
 )
 
+
+def hexdump(src, length=16, show=True):
+    print("--hexdump--")
+
+def receive_from(connection):
+    buffer = b""
+    connection.settimeout(5)
+    try:
+        while True:
+            data = connection.recv(4096)
+            if not data:
+                break
+
+            buffer += data
+    except:
+        pass
+
+    return buffer
+            
+
+def request_handler(buffer):
+    return buffer
+
+def response_handler(buffer):
+    return buffer
+
 def proxy_handler(client_socket, remote_host, remote_port, receive_first):
     print("---proxy handler----")
     remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     remote_socket.connect((remote_host, remote_port))
+
+    if receive_first:
+        remote_buffer = receive_from(remote_socket)
+        if len(remote_buffer):
+            print("[<==] Received %d bytes from remote" % len(remote_buffer))
+            hexdump(remote_buffer)
+
+            remote_buffer = response_handler(remote_buffer)
+            client_socket.send(remote_buffer)
+            print("[==>] Sent to local")
 
     
     
