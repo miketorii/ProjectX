6a39d22f06948db4fdaa75a015bd27027282a20a
miketorii <miketorii@gmail.com>
2025-06-08 00:26:22 +0000

    add hexdump

--- cybersecurity/proxy.py
+++ cybersecurity/proxy.py
@@ -9,7 +9,22 @@ HEX_FILTER = ''.join(
 
 def hexdump(src, length=16, show=True):
     print("--hexdump--")
+    if isinstance(src, bytes):
+        src = src.decode()
+    results = list()
+    for i in range(0, len(src), length):
+        word = str(str[i:i+length])
+        printable = word.translate(HEX_FILTER)
+        hexa = ' '.join([f'{ord(c)}:0xX'] for c in word)
+        hexwidth = length*3
+        results.append(f'{i:04x} {hexa:<{hexwidth}} {printable}')
+    if show:
+        for line in results:
+            print(line)
+    else:
+        return results
 
+    
 def receive_from(connection):
     buffer = b""
     connection.settimeout(5)
