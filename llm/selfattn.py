f331f59e533b566b27d805c2171b7bf59a6e512f
miketorii <miketorii@gmail.com>
2025-06-30 13:45:24 +0000

    add casual attention mask

--- llm/selfattn.py
+++ llm/selfattn.py
@@ -47,5 +47,26 @@ if __name__ == "__main__":
 
     print(result)
     
-    
+    ############################################
+    # casual attension mask
+    print("-------Casual Attention Mask------")
 
+    queries = selfattn.W_query(inputs)
+    keys = selfattn.W_key(inputs)
+    print(keys)
+    attn_scores = queries @ keys.T
+
+    attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+
+    print(attn_weights)
+    
+    context_length = attn_scores.shape[0]
+    mask_simple = torch.tril( torch.ones(context_length, context_length) )
+    print(mask_simple)
+    
+    masked_simple = attn_weights * mask_simple
+    print(masked_simple)
+
+    row_sums = masked_simple.sum(dim=1, keepdim=True)
+    masked_simple_norm = masked_simple / row_sums
+    print(masked_simple_norm)
