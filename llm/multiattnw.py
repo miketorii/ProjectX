acccbd47f28d5e16b64536a13e4ed46d5fb7af87
miketorii <miketorii@gmail.com>
2025-07-04 11:54:43 +0000

    add multi head attention

---
+++ llm/multiattnw.py
@@ -0,0 +1,76 @@
+import torch
+import torch.nn as nn
+
+##################################################3
+#
+#
+class CasualAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, qkv_bias=False):
+        print("---Casual Attention--")
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer('mask', torch.triu(torch.ones(context_length, context_length), diagonal=1))
+        
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+                             
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        attn_scores = queries @ keys.transpose(1,2)
+        attn_scores.masked_fill_( self.mask.bool()[:num_tokens, :num_tokens], -torch.inf)
+                             
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+                             
+        context_vec = attn_weights @ values
+
+        return context_vec
+
+class MultiHeadAttentionWrapper(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+        print("---Multi Head Attention Wrapper--")
+        super().__init__()
+        self.heads = nn.ModuleList(
+            [ CasualAttention(d_in, d_out, context_length, dropout, qkv_bias) for _ in range(num_heads) ]
+        )
+        
+    def forward(self, x):
+        return torch.cat( [head(x) for head in self.heads], dim=-1)
+    
+if __name__ == "__main__":
+    inputs = torch.tensor(
+        [[0.43, 0.15, 0.89], # Your     (x^1)
+         [0.55, 0.87, 0.66], # journey  (x^2)
+         [0.57, 0.85, 0.64], # starts   (x^3)
+         [0.22, 0.58, 0.33], # with     (x^4)
+         [0.77, 0.25, 0.10], # one      (x^5)
+         [0.05, 0.80, 0.55]] # step     (x^6)
+    )
+
+    print(inputs.shape)
+    print(inputs)
+    
+    batch = torch.stack((inputs, inputs), dim=0)
+    print(batch.shape)
+    print(batch)
+
+    torch.manual_seed(123)
+
+    d_in = 3
+    d_out = 2
+    print("d_in d_out = ", d_in, d_out)
+
+    context_length = batch.shape[1]
+    
+    mha = MultiHeadAttentionWrapper(d_in, d_out, context_length, 0.0, num_heads=2)
+    context_vecs = mha(batch)
+
+    print(context_vecs)
+    print(context_vecs.shape)
+    
