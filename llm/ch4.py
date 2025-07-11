34a1828de70afcecf765bb435ed4b7a6decd8da4
miketorii <miketorii@gmail.com>
2025-07-11 14:45:53 +0000

    add layer norm class

--- llm/ch4.py
+++ llm/ch4.py
@@ -33,7 +33,7 @@ class DummyGPTModel(nn.Module):
             *[DummyTransformerBlock(cfg) for _ in range(cfg["n_layers"]) ]
         )
 
-        self.final_norm = DummyLayerNorm(cfg["emb_dim"])
+        self.final_norm = LayerNorm(cfg["emb_dim"])
         self.out_head = nn.Linear( cfg["emb_dim"], cfg["vocab_size"], bias=False )
 
     def forward(self, in_idx):
@@ -62,13 +62,19 @@ class DummyTransformerBlock(nn.Module):
 #################################################
 #
 #
-class DummyLayerNorm(nn.Module):
-    def __init__(self, cfg):
-        print("---DummyLayerNorm---")
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        print("---LayerNorm---")
         super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
         
     def forward(self, x):
-        return x
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x-mean) / torch.sqrt(var*self.eps)
+        return self.scale * norm_x + self.shift
         
 #################################################
 #
