1bc1bcbfc4b19aaad11f8a2c34e96e1932de6526
miketorii <miketorii@gmail.com>
2025-07-24 11:31:50 +0000

    add cross entropy

--- llm/ch5.py
+++ llm/ch5.py
@@ -173,6 +173,50 @@ def calc_train(model, tokenizer):
     print("Training tokens;", train_tokens)
     print("Validatiaon tokens:", val_tokens)
     print("All tokens", train_tokens+val_tokens)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model.to(device)
+
+    torch.manual_seed(123)
+
+    with torch.no_grad():
+        train_loss = calc_loss_loader(train_loader, model, device, input_batch, target_batch)
+        val_loss = calc_loss_loader(val_loader, model, device, input_batch, target_batch)
+
+    print("Training loss:", train_loss)
+    print("Validation loss:", val_loss)
+
+############################################
+#
+#
+def calc_loss_batch(input_batch, target_batch, model, device):
+    input_batch, target_batch = input_batch.to(device), target_batch.to(device)
+    logits = model(input_batch)
+    loss = torch.nn.functional.cross_entropy( logits.flatten(0,1), target_batch.flatten() )    
+    return loss
+
+############################################
+#
+#    
+def calc_loss_loader(data_loader, model, device, input_batch, target_batch, num_batches=None):
+    total_loss = 0
+
+    if len(data_loader) == 0:
+        return float("nan")
+    elif num_batches is None:
+        num_batches = len(data_loader)
+    else:
+        num_batches = min(num_batches, len(data_loader))
+
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+        if i < num_batches:
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            total_loss += loss.item()
+        else:
+            break
+
+    return total_loss / num_batches
         
 ############################################
 #
