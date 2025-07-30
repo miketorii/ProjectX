2778ae4b50b92e84c959a8940322cefccd2fc4cc
miketorii <miketorii@gmail.com>
2025-07-30 11:44:39 +0000

    add k

--- llm/ch5test.py
+++ llm/ch5test.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 
+import matplotlib.pyplot as plt
+
 vocab = {
     "closer": 0,
     "every": 1,
@@ -13,18 +15,85 @@ vocab = {
     "you": 8    
 }
 
-inverse_vocab = {v: k for k, v in vocab.items()}
+########################################################
+#
+#
+def print_simpled_tokens(probas):
+    torch.manual_seed(123)
+    sample = [ torch.multinomial(probas, num_samples=1).item() for i in range(1_000) ]
+    sampled_ids = torch.bincount(torch.tensor(sample), minlength=len(probas) )
 
-next_token_logits = torch.tensor(
-    [4.51, 0.89, -1.90, 6.75, 1.63, -1.62, -1.89, 6.28, 1.79]
-)
+    for i, freq in enumerate(sampled_ids):
+        print(f"{freq} x {inverse_vocab[i]}")        
 
-probas = torch.softmax(next_token_logits, dim=0)
-next_token_id = torch.argmax(probas).item()
+    print("---------------------------------")
+    
+########################################################
+#
+#
+def softmax_with_temperature(logits, temperature):
+    scaled_logits = logits/temperature
+    return torch.softmax(scaled_logits, dim=0)
 
-print(inverse_vocab[next_token_id])
+########################################################
+#
+#
+if __name__ == "__main__":
+    inverse_vocab = {v: k for k, v in vocab.items()}
 
-torch.manual_seed(123)
-next_token_id = torch.multinomial(probas, num_samples=1).item()
-print(inverse_vocab[next_token_id])
+    next_token_logits = torch.tensor(
+        [4.51, 0.89, -1.90, 6.75, 1.63, -1.62, -1.89, 6.28, 1.79]
+    )
 
+    probas = torch.softmax(next_token_logits, dim=0)
+    next_token_id = torch.argmax(probas).item()
+
+    print("Next token is: ", inverse_vocab[next_token_id])
+
+    torch.manual_seed(123)
+    next_token_id = torch.multinomial(probas, num_samples=1).item()
+    print("Next token is(multinominal): ", inverse_vocab[next_token_id])
+
+    print_simpled_tokens(probas)
+    
+    temperatures = [1,0.1,5]
+    scaled_probas = [ softmax_with_temperature(next_token_logits, T) for T in temperatures ]
+
+    #######################################
+    x = torch.arange(len(vocab))
+    bar_width = 0.15
+
+    fig, ax = plt.subplots(figsize=(5,3))
+    for i, T in enumerate(temperatures):
+        rects = ax.bar( x+i*bar_width, scaled_probas[i], bar_width, label=f"Temperature = {T}")
+
+    ax.set_ylabel("Probability")
+    ax.set_xticks(x)
+    ax.set_xticklabels(vocab.keys(), rotation=90)
+    ax.legend()
+
+    plt.tight_layout()
+    plt.savefig("temperature-plot.pdf")
+    #######################################
+    
+    print_simpled_tokens(scaled_probas[1])
+    print_simpled_tokens(scaled_probas[2])
+    
+    #######################################
+    top_k = 3
+    top_logits, top_pos = torch.topk(next_token_logits, top_k)
+
+    print("Top logits: ", top_logits)
+    print("Top positions: ", top_pos)
+
+    new_logits = torch.where(
+        condition=next_token_logits < top_logits[-1],
+        input=torch.tensor(float("-inf")),
+        other=next_token_logits
+    )
+
+    print("New logits: ", new_logits)
+
+    topk_probas = torch.softmax(new_logits, dim=0)
+    print(topk_probas)
+    
