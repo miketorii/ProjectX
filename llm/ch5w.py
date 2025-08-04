01aa78283d503f40b586247cb279cd8493c1ee51
miketorii <miketorii@gmail.com>
2025-08-04 11:46:48 +0000

    add open ai weight

--- llm/ch5w.py
+++ llm/ch5w.py
@@ -493,9 +493,62 @@ def main_load_pthfile():
 
     print("---------------------------")
 
+############################################
+#
+#
+def main_load_openai_pthfile():
+    print("---load open ai weight----")
+
+    #file_name = "model.pth"
+    file_name = "gpt2-small-124M.pth"
+
+    url = f"https://huggingface.co/rasbt/gpt2-from-scratch-pytorch/resolve/main/{file_name}"
+
+    if not os.path.exists(file_name):
+        urllib.request.urlretrieve(url, file_name)
+        print(f"Downloaded to {file_name}")
+
+    model_configs = {
+        "gpt2-small (124M)": {"emb_dim":768, "n_layers": 12, "n_heads": 12},
+        "gpt2-medium (355M)": {"emb_dim":1024, "n_layers": 24, "n_heads": 16},
+        "gpt2-large (774M)": {"emb_dim":1280, "n_layers": 36, "n_heads": 20},
+        "gpt2-xl (1558M)": {"emb_dim":1600, "n_layers": 48, "n_heads": 25},        
+    }
+
+    model_name = "gpt2-small (124M)"
+    NEW_CONFIG = GPT_CONFIG_124M.copy()
+    NEW_CONFIG.update(model_configs[model_name])
+    NEW_CONFIG.update({"context_length":1024, "qkv_bias": True})
+
+    print(NEW_CONFIG)
+    
+    model = GPTModel(NEW_CONFIG)
+    model.load_state_dict(torch.load(file_name, weights_only=True))
+    model.eval()
+
+    model.to("cpu")
+
+    torch.manual_seed(123)
+    
+    start_context = "Every effort moves you"
+    tokenizer = tiktoken.get_encoding("gpt2")
+    
+    token_ids = generate(
+        model=model,
+        idx=text_to_token_ids(start_context, tokenizer).to("cpu"),
+        max_new_tokens=25,
+        context_size=NEW_CONFIG["context_length"],
+        top_k=50,
+        temperature=1.5
+    )
+
+    print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
+    print("---------------------------")
     
 if __name__ == "__main__":
-    main_load_pthfile()
+    main_load_openai_pthfile()
+    #main_load_pthfile()    
     #main_train_run()
     
     
