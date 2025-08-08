e63dcb972ff3c3e9595fd6bd08fd4a7a767a6112
miketorii <miketorii@gmail.com>
2025-08-08 11:14:09 +0000

    save csv

--- llm/ch6getdata.py
+++ llm/ch6getdata.py
@@ -5,7 +5,9 @@ from pathlib import Path
 
 import pandas as pd
 
-
+###############################################
+#
+#
 def download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path):
     if data_file_path.exists():
         return
@@ -20,7 +22,10 @@ def download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path):
     original_file_path = Path(extracted_path) / "SMSSpamCollection"
     os.rename(original_file_path, data_file_path)
     print(f"File downloaded and saved as {data_file_path}")
-        
+
+###############################################
+#
+#    
 def read_data():
     url = "https://archive.ics.uci.edu/static/public/228/sms+spam+collection.zip"
     zip_path = "sms_spam_collection.zip"
@@ -41,6 +46,9 @@ def read_data():
 
     return df
 
+###############################################
+#
+#
 def create_balanced_dataset(df):
     num_spam = df[ df["Label"] == "spam"  ].shape[0]
 
@@ -55,7 +63,31 @@ def create_balanced_dataset(df):
         
     return balanced_df
 
+###############################################
+#
+#
+def random_split(df, train_frac, validation_frac):
+    df = df.sample(frac=1, random_state=123).reset_index(drop=True)
+
+    train_end = int(len(df) * train_frac)
+    validation_end = train_end + int(len(df) * validation_frac)
+
+    train_df = df[:train_end]
+    validation_df = df[train_end:validation_end]
+    test_df = df[validation_end:]
+
+    return train_df, validation_df, test_df
+    
+###############################################
+#
+#
 if __name__ == "__main__":
     df = read_data()
-    create_balanced_dataset(df)
+    b_df = create_balanced_dataset(df)
+
+    train_df, validation_df, test_df = random_split(b_df, 0.7, 0.1)
+    train_df.to_csv("train.csv", index=None)
+    validation_df.to_csv("validation.csv", index=None)
+    test_df.to_csv("test.csv", index=None)
+    
     
