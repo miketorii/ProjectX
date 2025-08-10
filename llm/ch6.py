330fa77319aaabf7b36a7f289de800faaa7b50f0
miketorii <miketorii@gmail.com>
2025-08-10 11:41:54 +0000

    add dataloader

--- llm/ch6.py
+++ llm/ch6.py
@@ -5,6 +5,8 @@ from torch.utils.data import Dataset
 
 import pandas as pd
 
+from torch.utils.data import DataLoader
+
 ###################################################
 #
 #
@@ -81,4 +83,41 @@ if __name__ == "__main__":
     )
     print(test_dataset.max_length)    
 
+    num_workers = 0
+    batch_size = 8
+
+    torch.manual_seed(123)
+
+    train_loader = DataLoader(
+        dataset=train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        drop_last=True
+    )
+
+    val_loader = DataLoader(
+        dataset=val_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        drop_last=False
+    )
+
+    test_loader = DataLoader(
+        dataset=test_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        drop_last=False        
+    )    
     
+    for input_batch, target_batch in train_loader:
+        pass
+
+    print("input batch dimensions: ", input_batch.shape)
+    print("Label batch dimensions: ", target_batch.shape)
+
+    print(f"{len(train_loader)} training batches")
+    print(f"{len(val_loader)} validation batches")
+    print(f"{len(test_loader)} test batches")
