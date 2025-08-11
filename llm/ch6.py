80dc89ee911b39ae2a7b16401c6fb2672c80c42e
miketorii <miketorii@gmail.com>
2025-08-11 02:17:51 +0000

    add base config

--- llm/ch6.py
+++ llm/ch6.py
@@ -121,3 +121,29 @@ if __name__ == "__main__":
     print(f"{len(train_loader)} training batches")
     print(f"{len(val_loader)} validation batches")
     print(f"{len(test_loader)} test batches")
+
+    CHOOSE_MODEL = "gpt2-small (124M)"
+    INPUT_PROMPT = "Every effort moves"
+
+    BASE_CONFIG = {
+        "vocab_size": 50257,
+        "context_length": 1024,
+        "drop_rate": 0.0,
+        "qkv_bias": True
+    }
+
+    model_configs = {
+        "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+        "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+        "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+        "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+    }
+
+    BASE_CONFIG.update(model_configs[CHOOSE_MODEL])
+
+    assert train_dataset.max_length <= BASE_CONFIG["context_length"], (
+        f"Dataset length {train_dataset.max_length} exceeds model's context"
+        f"length {BASE_CONFIG['context_length']}. Reinitialize data set with"
+        f"max_length={BASE_CONFIG['context_length']}"
+    )
+    
