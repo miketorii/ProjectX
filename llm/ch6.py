1530e012b2aa2aeeeeea9546666f9e5ec386f545
miketorii <miketorii@gmail.com>
2025-08-13 00:29:49 +0000

    add last layer

--- llm/ch6.py
+++ llm/ch6.py
@@ -169,3 +169,31 @@ if __name__ == "__main__":
     )
 
     print(token_ids_to_text(token_ids, tokenizer))
+    print(model)
+
+    for param in model.parameters():
+        param.requires_grad = False
+
+    torch.manual_seed(123)
+
+    num_classes = 2
+    model.out_head = torch.nn.Linear(in_features=BASE_CONFIG["emb_dim"], out_features=num_classes)
+
+    for param in model.trf_blocks[-1].parameters():
+        param.requires_grad = True
+
+    for param in model.final_norm.parameters():
+        param.requires_grad = True
+
+    inputs = tokenizer.encode("Do you have time")
+    inputs = torch.tensor(inputs).unsqueeze(0)
+    print("Inputs: ", inputs)
+    print("Inputs dimensions: ", inputs.shape)
+
+    with torch.no_grad():
+        outputs = model(inputs)
+
+    print("Outputs: ", outputs)
+    print("Outputs dimensions: ", outputs.shape)
+
+    print("Last output token: ", outputs[:,-1,:])
