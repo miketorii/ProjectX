ff8767bcd5dd78e9e441383c1213bfc2eb823179
miketorii <miketorii@gmail.com>
2025-08-14 00:35:46 +0000

    add accuracy

--- llm/ch6.py
+++ llm/ch6.py
@@ -59,6 +59,35 @@ class SpamDataset(Dataset):
                 max_length = encoded_length
         return max_length
 
+###################################################
+#
+#
+def calc_accuracy_loader(data_loader, model, device, num_batches=None):
+    model.eval()
+    correct_predictions, num_examples = 0, 0
+
+    if num_batches is None:
+        num_batches = len(data_loader)
+    else:
+        num_batches = min(num_batches, len(data_loader))
+
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+        if i < num_batches:
+            input_batch, target_batch = input_batch.to(device), target_batch.to(device)
+
+            with torch.no_grad():
+                logits = model(input_batch)[:,-1,:]
+
+            predicted_labels = torch.argmax(logits, dim=-1)
+
+            num_examples += predicted_labels.shape[0]
+            correct_predictions += (predicted_labels == target_batch).sum().item()
+            
+        else:
+            break
+    
+
+    return correct_predictions / num_examples
 
 ###################################################
 #
@@ -197,3 +226,23 @@ if __name__ == "__main__":
     print("Outputs dimensions: ", outputs.shape)
 
     print("Last output token: ", outputs[:,-1,:])
+
+    probas = torch.softmax(outputs[:,-1,:], dim=-1)
+    label = torch.argmax(probas)
+    print("Class label : ", label.item())
+
+    logits = outputs[:, -1, :]
+    label = torch.argmax(logits)
+    print("Class label : ", label.item())
+
+    device = torch.device("cpu")
+
+    model.to(device)
+
+    train_accuracy = calc_accuracy_loader(train_loader, model, device, num_batches=10)
+    val_accuracy = calc_accuracy_loader(val_loader, model, device, num_batches=10)
+    test_accuracy = calc_accuracy_loader(test_loader, model, device, num_batches=10)
+    
+    print(f"Training accuracy: {train_accuracy*100:.2f}%")
+    print(f"Validation accuracy: {val_accuracy*100:.2f}%")
+    print(f"Test accuracy: {test_accuracy*100:.2f}%")    
