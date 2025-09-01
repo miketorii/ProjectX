a560835637d847375447aa13f3c1711c15b2b266
miketorii <miketorii@gmail.com>
2025-09-01 11:37:46 +0000

    add train classifier

--- llm/ch6.py
+++ llm/ch6.py
@@ -123,6 +123,40 @@ def calc_loss_loader(data_loader, model, device, num_batches=None):
 ###################################################
 #
 #
+def train_classifier_simple(model, train_loader, val_loader, optimizer, device, num_epochs, eval_freq, eval_iter):
+    train_losses, val_losses, train_accs, val_accs = [], [], [], []
+    examples_seen, global_step = 0, -1
+
+    for epoch in range(num_epochs):
+        model.train()
+
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            loas = calc_loss_batch(input_batch, target_batch, model, device)
+            loss.backward()
+            optimizer.step()
+            examples_seen += input_batch.shape[0]
+            global_step += 1
+
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(model, train_loader, val_loader, device, eval_iter)
+                train_losses.append(train_loss)
+                val_lossses.append(val_loss)
+                print(f"Ep {epoch+1} (Step {global_step:06d}): "
+                      f"Train loss {train_loss:.3f}, Val loss {val_loss:.3f}")
+
+        train_accuracy = calc_accuracy_loader(train_loader, model, device, num_batches=eval_iter)
+        val_accuracy = calc_accuracy_loader(val_loader, model, device, num_batches=eval_iter)
+        print(f"Training accuracy: {train_accuracy*100:.2f}% | ", end="")
+        print(f"Validation accuracy: {val_accuracy*100:.2f}%")
+        train_accs.append(train_accuracy)
+        val_accs.append(val_accuracy)
+
+    return train_losses, vall_losses, train_accs, val_accs, examples_seen
+
+###################################################
+#
+#
 if __name__ == "__main__":
     tokenizer = tiktoken.get_encoding("gpt2")
     print(tokenizer.encode("<|endoftext|>", allowed_special={"<|endoftext|>"}))
