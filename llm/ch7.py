13d8028e5c49ef2fccf92dcc73251434db4ba425
miketorii <miketorii@gmail.com>
2025-09-11 11:22:55 +0000

    add collate

--- llm/ch7.py
+++ llm/ch7.py
@@ -28,7 +28,8 @@ def download_and_load_file(file_path, url):
 #
 def format_input(entry):
     instruction_text = (
-        f"Below is an instruction"
+        f"Below is an instruction that describes a taks."
+        f"Write a response that appropriately completes the request."
         f"\n\n### Instruction:\n{entry['instruction']}"
     )
 
@@ -108,7 +109,39 @@ def custom_collate_draft_2(batch, pad_token_id=50256, device="cpu"):
 #######################################
 #
 #
+def custom_collate_fn(batch, pad_token_id=50256, ignore_index=-100, allowed_max_length=None, device="cpu"):
+    batch_max_length = max(len(item)+1 for item in batch)
+    inputs_lst, targets_lst = [], []
 
+    for item in batch:
+        new_item = item.copy()
+        new_item += [pad_token_id]
+
+        padded = (
+            new_item + [pad_token_id] * (batch_max_length - len(new_item))
+        )
+        
+        inputs = torch.tensor(padded[:-1])
+        targets = torch.tensor(padded[1:])
+
+        mask = targets == pad_token_id
+        indices = torch.nonzero(mask).squeeze()
+        if indices.numel() > 1:
+            targets[indices[1:]] = ignore_index
+
+        if allowed_max_length is not None:
+            inputs = inputs[:allowed_max_length]
+            targets = targets[:allowd_max_length]
+
+        inputs_lst.append(inputs)
+        targets_lst.append(targets)
+
+    inputs_tensor = torch.stack(inputs_lst).to(device)
+    targets_tensor = torch.stack(targets_lst).to(device)
+    
+    return inputs_tensor, targets_tensor    
+
+    
 #######################################
 #
 #
@@ -163,4 +196,10 @@ if __name__ == "__main__":
     inputs, targets = custom_collate_draft_2(batch)
     print(inputs)
     print(targets)
+
+    #################################################3
+    ##
+    inputs, targets = custom_collate_fn(batch)
+    print(inputs)
+    print(targets)    
     
