47a7897333da00e86fa4c27915ed810cac662cce
miketorii <miketorii@gmail.com>
2025-11-22 02:00:56 +0000

    add code2

---
+++ sql/code2.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Address
+(      name VARCHAR(32) NOT NULL,
+      phone_nbr	VARCHAR(32),
+      address	VARCHAR(32) NOT NULL,
+      sex	CHAR(4) NOT NULL,
+      age	INTEGER NOT NULL,
+      PRIMARY KEY (name));
+
+INSERT INTO Address VALUES('小川','080-333-1234','東京都','男',30);
+
+SELECT name, phone_nbr, address, sex, age FROM Address;
