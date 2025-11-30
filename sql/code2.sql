a2e8d1ffe961444d107f3c31aab8ac423a4091f6
miketorii <miketorii@gmail.com>
2025-11-30 01:30:46 +0000

    add count=1

--- sql/code2.sql
+++ sql/code2.sql
@@ -27,3 +27,5 @@ SELECT sex, COUNT(*) FROM Address GROUP BY sex;
 SELECT address, COUNT(*) FROM Address GROUP BY address;
 
 SELECT COUNT(*) FROM Address;
+
+SELECT address, COUNT(*) FROM Address GROUP BY address HAVING COUNT(*)=1;
