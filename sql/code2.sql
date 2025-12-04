3bcf37d94f460cb0abcbf8403db6acd33495d424
miketorii <miketorii@gmail.com>
2025-12-04 13:25:57 +0000

    add table

--- sql/code2.sql
+++ sql/code2.sql
@@ -35,3 +35,8 @@ SELECT name, phone_nbr, address, sex, age FROM Address ORDER BY age DESC;
 CREATE VIEW CountAddress (v_address, cnt) AS SELECT address, COUNT(*) FROM Address GROUP BY address;
 SELECT v_address, cnt FROM CountAddress;
 
+CREATE TABLE Address2(name VARCHAR(32) NOT NULL, phone_nbr VARCHAR(32), address VARCHAR(32) NOT NULL, sex CHAR(4) NOT NULL, age INTEGER NOT NULL, PRIMARY KEY (name) );
+
+INSERT INTO Address2 VALUES('小川', '080-333-XXXX', '東京都', '男', 30);
+
+SELECT * FROM Address2
