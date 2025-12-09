a331436d4898d0f4201e23ddbb963e21894c01f9
miketorii <miketorii@gmail.com>
2025-12-09 12:36:00 +0000

    add CASE

--- sql/code3.sql
+++ sql/code3.sql
@@ -26,3 +26,9 @@ CREATE TABLE Population
 	CONSTRAINT pk_pop PRIMARY KEY(prefecture, sex) );
 
 INSERT INTO Population VALUES('徳島', '1', 60);
+
+SELECT prefecture,
+       SUM(CASE WHEN sex = '1' THEN pop ELSE 0 END) AS pop_men,
+       SUM(CASE WHEN sex = '2' THEN pop ELSE 0 END) AS pop_wom
+FROM Population
+GROUP BY prefecture;
