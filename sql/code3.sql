86ba1f8a129a90ea5e72b10da80d6833d846aedf
miketorii <miketorii@gmail.com>
2025-12-15 13:05:46 +0000

    add IN

--- sql/code3.sql
+++ sql/code3.sql
@@ -95,3 +95,14 @@ SELECT key, name,
 WHERE (date_1 = '2013-11-01' AND flg_1='T')
    OR (date_2 = '2013-11-01' AND flg_2='T')
    OR (date_3 = '2013-11-01' AND flg_3='T');
+
+SELECT key, name,
+	date_1, flg_1,
+	date_2, flg_2,
+	date_3, flg_3
+  FROM ThreeElements
+ WHERE ('2013-11-01', 'T')
+	IN ( (date_1, flg_1),
+             (date_2, flg_2),
+	     (date_3, flg_3) );
+	     
