1de6dba57b3c93e73310052ceaafe50507f15d3a
miketorii <miketorii@gmail.com>
2025-12-24 11:52:51 +0000

    add order

--- sql/code4.sql
+++ sql/code4.sql
@@ -102,3 +102,15 @@ SELECT CASE 	WHEN weight / POWER(height/100, 2) < 18.5 THEN 'Thin'
 		ELSE NULL END;
 
 
+SELECT name, age,
+	CASE 	WHEN age < 20 THEN 'child'
+		WHEN age BETWEEN 20 AND 69 THEN 'adult'
+		WHEN age >= 70 THEN 'senior'
+	ELSE NULL END AS age_class,
+	RANK() OVER( PARTITION BY CASE 	WHEN age < 20 THEN 'child'
+					WHEN age BETWEEN 20 AND 69 THEN 'adult'
+					WHEN age >= 70 THEN 'senior'
+				   ELSE NULL END
+			ORDER BY age ) AS age_rank_in_class
+  FROM Persons
+ ORDER BY age_class, age_rank_in_class;
