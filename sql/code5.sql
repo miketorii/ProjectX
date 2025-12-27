a09754c9d4f75b75dd18f828e201a73e56550fe9
miketorii <miketorii@gmail.com>
2025-12-27 10:32:10 +0000

    add SIGN

--- sql/code5.sql
+++ sql/code5.sql
@@ -24,3 +24,14 @@ INSERT INTO Foo VALUES( 3, 300 );
 INSERT INTO Foo VALUES( 4, 400 );
 
 SELECT col_a FROM Foo WHERE p_key = 1
+
+INSERT INTO Sales2
+SELECT company, year, sale,
+	CASE SIGN(sale - MAX(sale) 
+			OVER( PARTITION BY company ORDER BY year
+				ROWS BETWEEN 1 PRECEDING AND 1 PRECEDING) )
+	WHEN 0 THEN '='
+	WHEN 1 THEN '+'
+	WHEN -1 THEN '-'
+	ELSE NULL END AS var
+  FROM Sales;
