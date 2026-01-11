5538a82cf3b11755a5b885bc45b72cf0c22a3e0c
miketorii <miketorii@gmail.com>
2026-01-11 02:36:26 +0000

    add join

--- sql/code6.sql
+++ sql/code6.sql
@@ -61,3 +61,11 @@ SELECT A.col_a, B.col_b, C.col_c
       ON A.col_a = B.col_b
         INNER JOIN Table_C C
           ON A.col_a = C.col_c;
+
+SELECT A.col_a, B.col_b, C.col_c
+  FROM Table_A A
+    INNER JOIN Table_B B
+      ON A.col_a = B.col_b
+        INNER JOIN Table_C C
+          ON A.col_a = C.col_c AND C.col_c = B.col_b;
+
