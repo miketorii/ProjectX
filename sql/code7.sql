cc0544173e0f5a7754212e3c2cfceb6be7712939
miketorii <miketorii@gmail.com>
2026-01-15 13:01:48 +0000

    add sub query

--- sql/code7.sql
+++ sql/code7.sql
@@ -14,3 +14,9 @@ SELECT R1.cust_id, R1.seq, R1.price
   ON R1.cust_id = R2.cust_id
  AND R1.seq = R2.min_seq;
  
+SELECT cust_id, seq, price
+  FROM Receipts R1
+ WHERE seq = ( SELECT MIN(seq)
+                 FROM Receipts R2
+                WHERE R1.cust_id = R2.cust_id);
+		
