f226e82d6e9ad78e14af8de333cfe6adcdab0324
miketorii <miketorii@gmail.com>
2026-01-16 12:42:56 +0000

    add window func

--- sql/code7.sql
+++ sql/code7.sql
@@ -20,3 +20,10 @@ SELECT cust_id, seq, price
                  FROM Receipts R2
                 WHERE R1.cust_id = R2.cust_id);
 		
+SELECT cust_id, seq, price
+  FROM (SELECT cust_id, seq, price, 
+               ROW_NUMBER()
+                OVER (PARTITION BY cust_id ORDER BY seq) AS row_seq 
+           FROM Receipts ) WORK
+ WHERE WORK.row_seq = 1;
+
