6ce3bf641e471efd7565a4e83d479dc4bed98a09
miketorii <miketorii@gmail.com>
2026-01-18 14:04:40 +0900

    add retry

--- mcptest/test.py
+++ mcptest/test.py
@@ -1,3 +1,45 @@
 import mcp
+import asyncio
 
-print(f'MCP version: {mcp.__version__}')
+#print(f'MCP version: {mcp.__version__}')
+
+##############################################
+#
+MAXRETRY = 4
+
+RET_SUCESS = '200'
+RET_ERROR = '404'
+
+##############################################
+#
+def getdata(count):
+    print(f'---get data--- {count}')
+    ret = RET_ERROR
+
+    if count == MAXRETRY-1:
+        ret = RET_SUCESS
+
+    print(f'---get data ret = {ret}')
+    return ret
+
+##############################################
+#
+async def request_func():
+    print('---request func---')
+
+    base_delay = 1.0
+    
+    retrycount = 0
+
+    for i in range(MAXRETRY):    
+        ret = getdata(retrycount)
+        retrycount += 1
+
+        delay = base_delay * (2 ** i)
+        print(f'delay={delay}')
+
+        await asyncio.sleep(delay)
+        
+if __name__ == '__main__':
+    asyncio.run(request_func())
+    
