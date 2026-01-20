88a70e68023c4bffe5de51ff7d28fd07e844281f
miketorii <miketorii@gmail.com>
2026-01-21 00:31:38 +0900

    add openweather

--- mcptest/openweather.py
+++ mcptest/openweather.py
@@ -1,9 +1,21 @@
+import os
+import httpx
+from typing import Any, Optional
 from mcp.server.fastmcp import FastMCP
+from dotenv import load_dotenv
 
+print('-----openweather----')
+
+load_dotenv()
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
+BASE_URL = os.getenv("OPENWEATHER_BASE_URL")
+
+print(API_KEY)
+print(BASE_URL)
 
 print('-----call FastMCP----')
 # サーバーの作成
-mcp = FastMCP("MyServer")
+mcp = FastMCP("openweather")
 
 @mcp.tool()
 def hello(name: str) -> str:
