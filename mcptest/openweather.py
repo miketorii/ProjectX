620e3349bc52a2c2cc3681dfae5a3b22d9a55279
miketorii <miketorii@gmail.com>
2026-01-20 15:37:00 +0000

    add comment

--- mcptest/openweather.py
+++ mcptest/openweather.py
@@ -11,7 +11,7 @@ API_KEY = os.getenv("OPENWEATHER_API_KEY")
 BASE_URL = os.getenv("OPENWEATHER_BASE_URL")
 
 print(API_KEY)
-print(BASE_URL)
+print(f"Using API base URL={BASE_URL}')
 
 print('-----call FastMCP----')
 # サーバーの作成
