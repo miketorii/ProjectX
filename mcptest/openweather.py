0906235e09f4b8679dedb1be34a0a8c7fba54dc5
miketorii <miketorii@gmail.com>
2026-01-22 21:28:38 +0900

    add call async

--- mcptest/openweather.py
+++ mcptest/openweather.py
@@ -3,6 +3,7 @@ import httpx
 from typing import Any, Optional
 from mcp.server.fastmcp import FastMCP
 from dotenv import load_dotenv
+import asyncio
 
 print('-----openweather----')
 
@@ -20,11 +21,47 @@ mcp = FastMCP("openweather")
 ##########################################
 #
 async def fetch_weather_data(city: str) -> Optional[dict[str, Any]]:
-    return None
+    print('---fetch weather data---')
+
+    tmpdata={"id": 1, "name": "Tokyo",
+             "weather": "Sunny",
+             "description": "This is wether report",
+             "main" : {
+                 "temp":28,
+                 "feels_like":10,
+                 "temp_min":0,
+                 "temp_max":100,
+                 "humidity":68,
+                 "pressure":200
+             },
+             "wind": "windy",
+             "speed":"100",
+             "deg":"50"}
+
+    return tmpdata
 
 ##########################################
 #
 def format_weather_response(data: dict[str, Any]) -> str:
+    print('-----format weather response------')
+
+    city_name = data.get("name","none")
+
+    main = data.get("main",{})
+    temp = main.get("temp",0)
+    feels_like = main.get("feels_like",0)
+    temp_min = main.get("temp_min", 0)
+    temp_max = main.get("temp_max", 0)    
+    humidity = main.get("humidity",0)
+    pressure = main.get("pressure",0)
+    
+    print(city_name)
+    print(temp)
+    print(feels_like)
+    print(temp_min)
+    print(temp_max)
+    print(humidity)
+    
     return "not implemented"
 
 ##########################################
@@ -41,22 +78,35 @@ async def get_weather(city: str) -> str:
         天気情報のフォーマットされた文字列
     """
 
+    print('----get_weather---')
+    
     if not city or not city.strip():
         return "エラー: 都市名を入力してください。"
 
     weather_data = await fetch_weather_data(city.strip())
 
+    print(weather_data)
+    
     if weather_data is None:
         return f"すみません。{city} の天気情報を取得できませんでした。都市名を確認してもう一度お試しください。"
     
     
     return format_weather_response(weather_data)
 
+##################################
+#
+async def funcmain():
+    city = 'Tokyo'
+#    print(city.strip())
+#    ret = await fetch_weather_data(city.strip())
+    
+    ret = await get_weather("Tokyo")
+    print(ret)    
+
 if __name__ == "__main__":
     print('-----main start----')
-    ret = get_weather("Tokyo")
-    print(ret)
+    asyncio.run(funcmain())
     
-    mcp.run()
+    #mcp.run()
     print('-----main end----')    
     
