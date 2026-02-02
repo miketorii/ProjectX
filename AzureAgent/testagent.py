956028206a78235b3374367ea2b9dbc2edbeea9b
miketorii <miketorii@gmail.com>
2026-02-02 23:15:58 +0900

    add agent

--- AzureAgent/testagent.py
+++ AzureAgent/testagent.py
@@ -34,7 +34,39 @@ def func_main():
         )
         print(f'Created agent ID: {agent.id}')
         
-    
+        thread = project_client.agents.threads.create()
+        print(f'Created thread ID: {thread.id}')
+
+        question = """Draw a graph for a line with a slope of 4
+        and y-intercept of 9 and provide the file to me?"""
+
+        message = project_client.agents.messages.create(
+            thread_id = thread.id,
+            role="user",
+            content = question,
+        )
+        print(f"Created message ID: {message['id']}")
+
+        run = project_client.agents.runs.create_and_process(
+            thread_id = thread.id,
+            agent_id = agent.id,
+            additional_instructions="""Please address the user as Jane Doe.
+            The user has a premium account.""",
+        )
+        print(f"Run finished with satus: {run.status}")
+
+        if run.status == "failed":
+            print(f"Run failed: {run.last_error}")
+
+        messages = project_client.agents.messages.list(thread_id=thread.id)
+        print(f"Messages: {messages}")
+
+        for message in messages:
+            print(f"Role: {message.role}, Content: {message.content}")
+            for this_content in message.content:
+                print(f"Content Type: {this_content.type}, Conetnt Data: {this_content}")
+                
+        
     print('--------End func_main--------')
     
 if __name__ == '__main__':
