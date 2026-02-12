86811f1bfa3b238803f2cd17211906e9541d1f45
miketorii <miketorii@gmail.com>
2026-02-12 13:22:48 +0000

    add sub query

--- sql/code7.sql
+++ sql/code7.sql
@@ -60,3 +60,14 @@ SELECT TMP_MIN.cust_id,
        ) TMP_MAX
 
   ON TMP_MIN.cust_id = TMP_MAX.cust_id;
+
+SELECT cust_id, 
+       SUM(CASE WHEN min_seq=1 THEN price ELSE 0 END) 
+       - SUM(CASE WHEN max_seq=1 THEN price ELSE 0 END) AS diff
+  FROM (SELECT cust_id, price,
+               ROW_NUMBER() OVER (PARTITION BY cust_id ORDER BY seq) AS min_seq,
+               ROW_NUMBER() OVER (PARTITION BY cust_id ORDER BY seq DESC) AS max_seq
+        FROM Receipts) WORK
+ WHERE WORK.min_seq=1 OR WORK.max_seq=1
+ GROUP BY cust_id;
+
