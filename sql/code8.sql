ed593fa0607e1197b3fab4b06fe0cae4322712df
miketorii <miketorii@gmail.com>
2026-02-16 13:19:37 +0000

    add 2 keys case

--- sql/code8.sql
+++ sql/code8.sql
@@ -13,3 +13,18 @@ SELECT student_id,
       WHERE W2.student_id <= W1.student_id) AS seq
  FROM Weights W1;
 
+CREATE TABLE Weights2
+(class       INTEGER NOT NULL,
+ student_id  CHAR(4) NOT NULL,
+ weight      INTEGER NOT NULL,
+ PRIMARY KEY(class, student_id));
+
+INSERT INTO Weights2 VALUES(1, '100', 50);
+
+SELECT class, student_id,
+   (SELECT COUNT(*)
+      FROM Weights2 W2
+     WHERE (W2.class, W2.student_id)
+             <= (W1.class, W1.student_id) ) AS seq
+ FROM Weights2 W1;
+ 
