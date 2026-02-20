d90fb1445dc93bffa649ec1257e165caceac0e59
miketorii <miketorii@gmail.com>
2026-02-20 14:30:02 +0000

    add demo file

---
+++ langchain/demo1.py
@@ -0,0 +1,47 @@
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain_openai import AzureChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel, Field
+
+print("----------start----------------")
+
+class Recipe(BaseModel):
+    ingredients: list[str] = Field(description="ingredients of the dish")
+    steps: list[str] = Field(description="steps to make the dish")
+
+output_parser = PydanticOutputParser(pydantic_object=Recipe)
+    
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "ユーザが入力した項目の市場動向を日本語で考えてください\n\n{format_instructions}"),  
+        ( "human", "{dish}"),
+    ]
+)
+
+prompt_with_format_instructions = prompt.partial(
+    format_instructions=output_parser.get_format_instructions()
+)
+
+model = AzureChatOpenAI(
+    azure_deployment="my-gpt-4o-1",
+    api_version="2024-08-01-preview",
+    temperature=0.5,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+).bind( response_format={"type":"json_object"} )
+
+chain = prompt_with_format_instructions | model | output_parser
+
+recipe = chain.invoke({"dish":"ARM PC"})
+
+print(type(recipe))
+print(recipe)
+
+print("======================================")
+print(recipe.ingredients)
+print("======================================")
+print(recipe.steps)
+
+print("----------end------------------")
+
