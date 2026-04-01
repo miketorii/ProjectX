0a59a388a2b5838283c476f050ebbe3c79552f3a
miketorii <miketorii@gmail.com>
2026-04-01 12:38:29 +0000

    add load from google storage

--- langchain/btest13.py
+++ langchain/btest13.py
@@ -3,6 +3,8 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.document_loaders.csv_loader import CSVLoader
 import glob
 
+from langchain.text_splitter import CharacterTextSplitter
+
 all_documents = []
 
 loader = PyPDFLoader("data/test.pdf")
@@ -18,5 +20,29 @@ for csv_file in csv_files:
     loader = CSVLoader(file_path=csv_file)
     data = loader.load()
     all_documents.extend(data)
+
+print(all_documents)
+
+text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=200, chunk_overlap=0
+)
+
+urls = [
+    "https://storage.googleapis.com/oreilly-content/NutriFusion%20Foods%20Marketing%20Plan%202022.docx",
+    "https://storage.googleapis.com/oreilly-content/NutriFusion%20Foods%20Marketing%20Plan%202023.docx",
+]
+
+docs = []
+
+for url in urls:
+    loader = Docx2txtLoader(url)
+    pages = loader.load()
+    chunks = text_splitter.split_documents(pages)
+
+    for chunk in chunks:
+        chunk.metadata["source"] = "NutriFusion Foods Marketing Plan - 2022/2023"
+    docs.extend(chunks)
+
+all_documents.extend(docs)
 
 print(all_documents)
