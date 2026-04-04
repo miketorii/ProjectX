5dd323cb28533273f1511c0f4819cd2da93e3c5f
miketorii <miketorii@gmail.com>
2026-04-04 15:04:11 +0000

    add function

--- langchain/btest15.py
+++ langchain/btest15.py
@@ -1,4 +1,22 @@
+from langchain.output_parsers.openai_tools import PydanticToolsParser
+from langchain_core.utils.function_calling import convert_to_openai_tool
+from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel, Field
+from langchain_openai import AzureChatOpenAI
+from typing import Optional
+
 import json
 from os import getenv
 
+class Article(BaseModel):
+    points: str = Field(..., description="Key points from the article")
+    constrain_points: Optional[str] = Field( None, description="Any constrain points acknowledge in the article")
+    author: Optional[str] = Field( None, description="Author of the article")
+
+    
+print("-------start----------")
+
+
+
+print("-------end----------")
 
