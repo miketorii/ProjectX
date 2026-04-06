3e003eeb15fe06750b51bb72c0e8dd7919f0ebf6
miketorii <miketorii@gmail.com>
2026-04-06 13:27:53 +0000

    add function

--- langchain/btest15.py
+++ langchain/btest15.py
@@ -31,7 +31,41 @@ prompt = ChatPromptTemplate.from_messages(
     }
 )
 
+model = AzureChatOpenAI(
+    azure_deployment="MyModel",  # or your deployment
+    api_version="2024-02-01",  # or your api version
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    # other params...
+)
 
+#client = AzureOpenAI(
+#    azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"),
+#    api_key=os.getenv("AZURE_OPENAI_KEY"),
+#    api_version="2024-08-01-preview"
+#)
+
+pydantic_schemas = [Article]
+
+tools = [convert_to_openai_tool(p) for p in pydantic_schemas]
+
+#for p in pydantic_schemas:
+#    print(p)
+[print(p) for p in pydantic_schemas]
+
+model = model.bind_tools(tools=tools)
+
+chain = prompt | model | PydanticToolsParser(tools=pydantic_schemas)
+
+result = chain.invoke(
+    {
+        "input":"""In the recent article titled 'AI adaption in industry', key points addressed include the glowing interest in AI in various sectors, the increase in AI research, and the need for responsible AI. However, the author, Dr. Jane Smith, acknowledges a contrarian view --- that without stringent regulations, AI may pose high risks."""
+    }
+)
+
+print(result)
 
 print("-------end----------")
 
