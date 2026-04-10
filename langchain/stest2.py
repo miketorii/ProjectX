e3253d96acb822746026f6e2325c90a0edaa73cc
miketorii <miketorii@gmail.com>
2026-04-10 13:26:46 +0000

    add csv agent

---
+++ langchain/stest2.py
@@ -0,0 +1,47 @@
+import os
+
+from openai import AzureOpenAI
+from langchain_openai import AzureChatOpenAI
+
+#from langchain.agents.agent_types import AgentType
+from langchain_experimental.agents.agent_toolkits import create_csv_agent
+    
+######################################
+#
+#    
+print("----------start----------------")
+
+model = AzureChatOpenAI(
+    azure_deployment="MyModel",  # or your deployment
+    api_version="2024-02-01",  # or your api version
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    # other params...
+)
+
+#client = AzureOpenAI(
+#    azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"),
+#    api_key=os.getenv("AZURE_OPENAI_KEY"),
+#    api_version="2024-08-01-preview"
+#)
+
+agent = create_csv_agent(
+    model,
+    "data/heart_disease_uci.csv",
+    verbose=True,
+    allow_dangerous_code=True
+)
+
+response = agent.run("How many rows of data are in the file?")
+
+print(response)
+
+print("-------------------------------")
+print("----------end------------------")
+
+
+
+
+
