250989548a051b7039e601316e25473520ac6315
miketorii <miketorii@gmail.com>
2026-04-24 12:18:45 +0000

    modified regular expression

--- langchainexp/stest6.py
+++ langchainexp/stest6.py
@@ -13,7 +13,9 @@ def extract_last_action_and_input(text):
 #    action_pattern = re.compile(r"(?i)action\s*:\s*([^\n]+)", re.MULTILINE)
 #    action_input_pattern = re.compile(r"(?i)action\s*_*input\s*:\s*([^\n]+)", re.MULTILINE)
 
-    action_pattern = re.compile(r"(?i)action\s*:\s*([^\n]+)", re.MULTILINE)
+    #action_pattern = re.compile(r"(?i)action\s*:\s*([^\n]+)", re.MULTILINE)
+    action_pattern = re.compile(r"(?i)action\s*:\s*(.*(?<!\s))", re.MULTILINE)
+
     action_input_pattern = re.compile(
         r"(?i)action\s*_*input\s*:\s*([^\n]+)", re.MULTILINE
     )
@@ -24,7 +26,6 @@ def extract_last_action_and_input(text):
     last_action = actions[-1] if actions else None
     last_action_input = action_inputs[-1] if action_inputs else None
 
-    print(last_action+"345")
     print("Last Action: ", last_action)
     print("Last Action Input: ", last_action_input)
 
@@ -116,6 +117,11 @@ tools = {}
 #    "description": "Searches on google for a query"
 #}
 
+#tools["search_on_google"] = {
+#    "function":search_on_google,
+#    "description":"Searches on google for a query",
+#}
+
 tools["search_on_google"] = {
     "function": search_on_google,
     "description": "Searches on google for a query",
@@ -127,15 +133,19 @@ Use chain of thought reasoning to solve through the problem, using the following
 
 1. Observe the original question:
 original_question: original_problem_text
+
 2. Create an observation with the following pattern:
 observation: observation_text
+
 3. Create a thought based on the observation with the following pattern:
 thought: thought_text
+
 4. Use tools to act on the thought with the following pattern:
 action: tool_name
 action_input: tool_input
 
-Do not guess or assume the tool results. Instead, provide a structured output that includes the action and action_input.
+Do not guess or assume the tool results.
+Instead, provide a structured output that includes the action and action_input.
 
 You have access to the following tools: {tools}.
 
@@ -151,13 +161,6 @@ print(model_output)
 
 tool_name = extract_last_action_and_input(model_output.content)["action"]
 tool_input = extract_last_action_and_input(model_output.content)["action_input"]
-print(tool_name+"123")
-print(tool_input)
-tool_result = tools["search_on_google"]["function"](tool_input)
-print(tool_result)
-
-#tool_result = tools[tool_name]["function"](tool_input)
-#tool_result = ""
 tool_result = tools[tool_name]["function"](tool_input)
 
 print(
