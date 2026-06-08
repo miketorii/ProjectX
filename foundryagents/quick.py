491ccd645f8ff950746fb6e94e39ac9fd1abc747
miketorii <miketorii@gmail.com>
2026-06-08 20:53:18 +0900

    add tool

--- foundryagents/quick.py
+++ foundryagents/quick.py
@@ -2,14 +2,27 @@ import asyncio
 import os
 from dotenv import load_dotenv
 
-from agent_framework import Agent
+from random import randint
+from typing import Annotated
+
+from agent_framework import Agent, tool
 from agent_framework.foundry import FoundryChatClient
 from azure.identity import AzureCliCredential
+
+from pydantic import Field
+
 
 load_dotenv()
 
 FONDRY_MODE=os.getenv('FONDRY_NAME')
 FONDRY_PROJECT_ENDPOINT=os.getenv("FONDRY_PROJECT_ENDPOINT")
+
+@tool(approval_mode="never_require")
+def get_weather(
+        locaiton: Annotated[str, Field(description="The location to get the weather for.")]
+) -> str:
+    conditions = ["sunny", 'cloudy', "rainy", "stormy"]
+    return f"The weather in {locaiton} is {conditions[randint(0,3)]} with a high of {randint(10,30)}C."
 
 async def main() -> None:
     print('---IN main--')
@@ -21,9 +34,12 @@ async def main() -> None:
             credential=AzureCliCredential(),
         ),
         instructions="you are a helpful assistant.",
+        tools=get_weather,
     )
 
-    result = await agent.run("What is the capital of France?")
+#    question = "What is the capital of France?"
+    question = "What is the weather like in Seattle?"        
+    result = await agent.run(question)
     print(f"Agent: {result}")
 
 if __name__ == "__main__":
