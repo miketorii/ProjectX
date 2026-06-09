2c88b0ab59edc161d93b06c8a6ba15c10816cc46
miketorii <miketorii@gmail.com>
2026-06-09 22:15:02 +0900

    add prompt agent

---
+++ foundryagents/promptagent.py
@@ -0,0 +1,40 @@
+import asyncio
+import os
+from dotenv import load_dotenv
+
+#from azure.identity import AzureCliCredential
+
+from azure.identity import DefaultAzureCredential
+from azure.ai.projects import AIProjectClient
+from azure.ai.projects.models import PromptAgentDefinition
+
+load_dotenv()
+
+FONDRY_MODE=os.getenv('FONDRY_NAME')
+FONDRY_PROJECT_ENDPOINT=os.getenv("FONDRY_PROJECT_ENDPOINT")
+AGENT_NAME=os.getenv("AGENT_NAME")
+
+async def main() -> None:
+    print('---IN main--')
+
+    project = AIProjectClient(
+        endpoint=os.environ["FONDRY_PROJECT_ENDPOINT"],        
+#        credential=AzureCliCredential(),
+        credential=DefaultAzureCredential(),                
+    )
+
+    agent = project.agents.create_version(
+        agent_name=AGENT_NAME,
+        definition=PromptAgentDefinition(
+            model=os.environ["FONDRY_MODEL"],
+        instructions="you are a helpful assistant that answers general questions.",            
+        )
+    )
+
+    print(f"Agent created id: {agent.id}, name: {agent.name}, version: {agent.version}")
+
+if __name__ == "__main__":
+    print('---------start------------')
+    asyncio.run(main())
+    
+
