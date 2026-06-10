c95ddf5890102c81802f33053bd44081597e531b
miketorii <miketorii@gmail.com>
2026-06-10 22:19:05 +0900

    add client

---
+++ foundryagents/promptclient.py
@@ -0,0 +1,45 @@
+import asyncio
+import os
+from dotenv import load_dotenv
+
+from azure.identity import DefaultAzureCredential
+from azure.ai.projects import AIProjectClient
+from azure.ai.projects.models import PromptAgentDefinition
+
+load_dotenv()
+
+FONDRY_MODE=os.getenv('FONDRY_NAME')
+FONDRY_PROJECT_ENDPOINT=os.getenv("FONDRY_PROJECT_ENDPOINT")
+AGENT_NAME=os.getenv("AGENT_NAME")
+
+async def main() -> None:
+    print('---IN main--')
+
+    project = AIProjectClient(
+        endpoint=os.environ["FONDRY_PROJECT_ENDPOINT"],        
+        credential=DefaultAzureCredential(),                
+    )
+    openai = project.get_openai_client()
+
+    conversation = openai.conversations.create()
+
+    response = openai.responses.create(
+        conversation=conversation.id,
+        extra_body={"agent_reference": {"name": AGENT_NAME, "type": "agent_reference"} },
+        input="What is the size of France in square miles?"
+    )
+    print(response.output_text)
+
+    response = openai.responses.create(
+        conversation=conversation.id,
+        extra_body={"agent_reference": {"name": AGENT_NAME, "type": "agent_reference"} },
+        input="And shat is the capital city?"
+    )
+    print(response.output_text)
+    
+
+if __name__ == "__main__":
+    print('---------start------------')
+    asyncio.run(main())
+    
+
