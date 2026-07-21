0cf643f1c7f8ea4c7ecd414d6401934b12035765
miketorii <miketorii@gmail.com>
2026-07-21 11:57:58 +0000

    add alpha

--- rein/non_stationary.py
+++ rein/non_stationary.py
@@ -14,11 +14,40 @@ class NonStatBadit:
             return 1
         else:
             return 0
+
+class AlphaAgent:
+    def __init__(self, epsilon, alpha, actions=10):
+        self.epsilon = epsilon
+        self.Qs = np.zeros(actions)
+        self.alpha = alpha
+
+    def update(self, action, reward):
+        self.Qs[action] += (reward - self.Qs[action]) * self.alpha
+
+    def get_action(self):
+        if np.random.rand() < self.epsilon:
+            return np.random.randint(0, len(self.Qs))
+        return np.argmax(self.Qs)
+    
         
 if __name__ == "__main__":
     print("--------start----------")
+    runs = 200
+    steps = 1000
+    epsilon = 0.1
+    alpha = 0.8
+    agent_types = ['simple average','alpha const update']
+    results = {}
+    
     nonstat = NonStatBadit()
     ret = nonstat.play(1)
     print(ret)
+
+    agent = AlphaAgent(epsilon, alpha)
+    actionnum = agent.get_action()
+    print(actionnum)
+    reward = 5
+    agent.update(actionnum, reward)
+    
     print("--------end----------")    
     
