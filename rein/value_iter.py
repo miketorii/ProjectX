56e0e3146bcefcac3d7b3a1acfa79e8bffe51381
miketorii <miketorii@gmail.com>
2026-08-05 12:23:33 +0000

    add value iter

--- rein/value_iter.py
+++ rein/value_iter.py
@@ -3,7 +3,7 @@ from gridworld import GridWorld
 from policy_iter import greedy_policy
 
 def value_iter_onestep(V, env, gamma):
-    print("---value iter onestep---")
+#    print("---value iter onestep---")
     for state in env.states():
         if state == env.goal_state:
             V[state] = 0
@@ -24,6 +24,20 @@ def value_iter_onestep(V, env, gamma):
 
 def value_iter(V, env, gamma, threshold=0.001, is_render=True):
     print('---value iter---')
+    while True:
+        old_V = V.copy()
+        V = value_iter_onestep(V, env, gamma)
+
+        delta = 0
+        for state in V.keys():
+            t = abs(V[state]-old_V[state])
+            if delta < t:
+                delta = t
+
+        if delta < threshold:
+            break
+
+    return V
 
 if __name__ == "__main__":
     print("----start-----")
@@ -31,13 +45,15 @@ if __name__ == "__main__":
     env = GridWorld()
     gamma = 0.9
 
-    V = value_iter_onestep(V, env, gamma)
-    print(V)
+#    V = value_iter_onestep(V, env, gamma)
+#    print(V)
     
     V = value_iter(V, env, gamma)
-
-#    pi = greedy_policy(V, env, gamma)
-
+    print(V)
+    
+    pi = greedy_policy(V, env, gamma)
+    print(pi)
+    
     #env.render_v(V, pi)
 
     print("----end-----")    
