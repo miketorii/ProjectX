428453d0e2148e9a4dd0ad86ee013608e208756f
miketorii <miketorii@gmail.com>
2026-08-28 22:31:38 +0900

    add DQN

--- rein/dqn.py
+++ rein/dqn.py
@@ -53,27 +53,100 @@ class QNet(Model):
 ###################################################################
 #
 #
+class DQNAgent:
+    def __init__(self):
+        self.gamma = 0.98
+        self.lr = 0.0005
+        self.epsilon = 0.1
+        self.buffer_size = 10000
+        self.batch_size = 32
+        self.action_size = 2
 
+        self.replay_buffer = ReplayBuffer(self.buffer_size, self.batch_size)
+        self.qnet = QNet(self.action_size)
+        self.qnet_target = QNet(self.action_size)
+        self.optimizer = optimizers.Adam(self.lr)
+        self.optimizer.setup(self.qnet)
 
+    def get_action(self, state):
+        if np.random.rand() < self.epsilon:
+            return np.random.choice(self.action_size)
+        else:
+            state = state[np.newaxis,:]
+            qs = self.qnet(state)
+            return qs.data.argmax()
+
+    def update(self, state, action, reward, next_state, done):
+        self.replay_buffer.add(state, action, reward, next_state, done)
+        if len(self.replay_buffer) < self.batch_size:
+            return
+
+        state, action, reward, next_state, done = self.replay_buffer.get_batch()
+        qs = self.qnet(state)
+        q = qs[np.arange(self.batch_size), action]
+
+        next_qs = self.qnet_target(next_state)
+        next_q = next_qs.max(axis=1)
+        next_q.unchain()
+        target = reward + (1-done) * self.gamma * next_q
+
+        loss = F.mean_squared_error(q, target)
+        
+        self.qnet.cleargrads()
+        loss.backward()
+        self.optimizer.update()
+        
+    def sync_qnet(self):
+        self.qnet_target = copy.deepcopy(self.qnet)
+        
+###################################################################
+#
+#
 if __name__ == "__main__":
     print("-------------start---------------")
 
+    episodes = 300
+    sync_interval = 20   
     env = gym.make('CartPole-v1', render_mode='human')
-    replay_buffer = ReplayBuffer(buffer_size=10000, batch_size=32)
-
-    for episode in range(10):
+    agent = DQNAgent()
+    reward_history = []
+    
+    for episode in range(episodes):
         state, info = env.reset()
         done = False
+        total_reward = 0
         truncated = False
 
         while not (done or truncated):
-            action = 0
+            action = agent.get_action(state)
             next_state, reward, truncated, done, info = env.step(action)
-            replay_buffer.add(state, action, reward, next_state, done)
+
+            agent.update(state, action, reward, next_state, done)
             state = next_state
+            total_reward += reward
 
-    state, action, reward, next_state, done = replay_buffer.get_batch()
+        if episode % sync_interval == 0:
+            agent.sync_qnet()
 
+        reward_history.append(total_reward)
+        if episode % 10 == 0:
+            print("episode:{}, total reward: {}".format(episode, total_reward))
+
+
+    agent.epsilon = 0
+    state = env.reset()
+    done = False
+    total_reward = 0
+    truncated = False
+    
+    while not (done or truncated):
+        action = agent.get_action(state)
+        next_state, reward, truncated, done, info = env.step(action)
+        state = next_state
+        total_reward += reward
+
+    print('Total Reward:', total_reward)
+    
     print(state.shape)
     print(action.shape)
     print(reward.shape)
