268a896a067603bfab224d1289ac595aeb9c858f
miketorii <miketorii@gmail.com>
2026-08-29 15:46:38 +0000

    add pg

---
+++ rein/simple_pg.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+from dezero import Model
+from dezero import optimizers
+import dezero.functions as F
+import dezero.layers as L
+
+if __name__ == '__main__':
+    print("---------------start---------------")
