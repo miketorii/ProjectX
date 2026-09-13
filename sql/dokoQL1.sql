a03d2bdb339f8a7f079153e6872e286eceb44cdd
miketorii <miketorii@gmail.com>
2026-09-13 19:46:24 +0900

    insert

--- sql/dokoQL1.sql
+++ sql/dokoQL1.sql
@@ -2,6 +2,7 @@ select メモ from 家計簿
 select 日付, 費目, 出金額 from 家計簿;
 select * from 家計簿;
 select 日付, 費目, 出金額 from 家計簿 where 出金額 > 3000;
+insert into 家計簿 values ('2018-02-25','居住費','3月の家賃',0,85000);
 
 
 
