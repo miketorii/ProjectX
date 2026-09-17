a0dbbe08a0797ec8496f164f25a6350e383b4d60
miketorii <miketorii@gmail.com>
2026-09-17 21:36:22 +0900

    update

--- sql/dokoQL1.sql
+++ sql/dokoQL1.sql
@@ -3,6 +3,8 @@ select 日付, 費目, 出金額 from 家計簿;
 select * from 家計簿;
 select 日付, 費目, 出金額 from 家計簿 where 出金額 > 3000;
 insert into 家計簿 values ('2018-02-25','居住費','3月の家賃',0,85000);
+update 家計簿 set 出金額=90000 where 日付='2024-02-11';
+
 
 
 
