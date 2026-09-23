215ef87b5b967c8df38730cb5eb36697c121e112
miketorii <miketorii@gmail.com>
2026-09-23 15:36:55 +0900

    as

--- sql/dokoQL1.sql
+++ sql/dokoQL1.sql
@@ -14,6 +14,10 @@ delete from 家計簿 where 日付='2024-02-14';
 /*                  chaper 2                     */
 /*************************************************/
 
+select 費目 as ITEM, 入金額 as RECEIVE, 出金額 as PAY
+ from 家計簿 as MONEYBOOK
+where 費目= '給料';
+
 
 
 
